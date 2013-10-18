21923f838938068236bad1a5f295995ad03d5514
Nathan Wolf <nathan@WolfBook.local>
2013-10-18 10:45:52 -0700

    Add fire parameter to arrow, and fix arrow spread.
    Still hurts the player occasionally :\

--- src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/ArrowSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/ArrowSpell.java
@@ -3,7 +3,9 @@ package com.elmakers.mine.bukkit.plugins.magic.spells;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
+import org.bukkit.util.Vector;
 
 import com.elmakers.mine.bukkit.plugins.magic.Spell;
 import com.elmakers.mine.bukkit.utilities.borrowed.ConfigurationNode;
@@ -15,15 +17,36 @@ public class ArrowSpell extends Spell
 	{
 	    int arrowCount = 1;
 	    arrowCount = parameters.getInt("count", arrowCount);
-		
+	    boolean useFire = parameters.getBoolean("fire", true);
+	    
+	    Arrow firstArrow = null;
+	    float speed = 3;
+	    float spread = 0.1f;
 		for (int ai = 0; ai < arrowCount; ai++)
 		{
-		    Arrow arrow = player.launchProjectile(Arrow.class);
+		    Arrow arrow = null;
+		    if (firstArrow == null) {
+		    	arrow = player.launchProjectile(Arrow.class);
+		    	firstArrow = arrow;
+		    } else {
+		    	Location location = firstArrow.getLocation();
+		    	Vector velocity = firstArrow.getVelocity();
+		    	location.setX(location.getX() + velocity.getX() * (1 + Math.random() * arrowCount));
+		    	location.setY(location.getY() + velocity.getY() * (1 + Math.random() * arrowCount));
+		    	location.setZ(location.getZ() + velocity.getZ() * (1 + Math.random() * arrowCount));
+		    	arrow = player.getWorld().spawnArrow(location, velocity, speed, spread);
+		    }
+		    
 		    if (arrow == null)
 		    {
 		        sendMessage(player, "One of your arrows fizzled");
 		        return false;
 		    }
+		    
+		    if (useFire) {
+		    	arrow.setFireTicks(300);
+		    }
+		    
 		    // Hackily make this an infinite arrow
 		    try {
 		    	Method getHandleMethod = arrow.getClass().getMethod("getHandle");
@@ -33,7 +56,7 @@ public class ArrowSpell extends Spell
 		    } catch (Throwable ex) {
 		    	ex.printStackTrace();
 		    }
-		    arrow.setTicksLived(1150);
+		    arrow.setTicksLived(300);
 		}
 	
 		castMessage(player, "You fire some magical arrows");
