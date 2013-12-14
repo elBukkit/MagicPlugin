36e03f894f86f600891683b3024586d186725a38
Nathan Wolf <nathan@WolfBook.local>
2013-12-13 23:39:52 -0800

    Allow getting a url map by id

--- src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/CameraSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/CameraSpell.java
@@ -1,5 +1,7 @@
 package com.elmakers.mine.bukkit.plugins.magic.spells;
 
+import org.bukkit.Effect;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -14,15 +16,24 @@ public class CameraSpell extends Spell
 	@Override
 	public SpellResult onCast(ConfigurationNode parameters) 
 	{
-		// Check for special case url
 		ItemStack newMapItem = null;
-		String url = parameters.getString("url");
-		if (url != null) {
-			int x = parameters.getInt("x", 0);
-			int y = parameters.getInt("y", 0);
-			int width = parameters.getInt("width", 0);
-			int height = parameters.getInt("height", 0);
-			newMapItem = URLMapRenderer.getURLItem(url, x, y, width, height, "Photo");
+		
+		// Check for special case id
+		if (parameters.containsKey("id"))
+		{
+			newMapItem = new ItemStack(Material.MAP, 1, (short)parameters.getInt("id", 0));
+		}
+		
+		// Check for special case url
+		if (newMapItem == null) {
+			String url = parameters.getString("url");
+			if (url != null) {
+				int x = parameters.getInt("x", 0);
+				int y = parameters.getInt("y", 0);
+				int width = parameters.getInt("width", 0);
+				int height = parameters.getInt("height", 0);
+				newMapItem = URLMapRenderer.getURLItem(url, x, y, width, height, "Photo");
+			}
 		}
 		
 		if (newMapItem == null) {
@@ -53,6 +64,7 @@ public class CameraSpell extends Spell
 			return SpellResult.FAILURE;
 		}
 		player.getWorld().dropItemNaturally(player.getLocation(), newMapItem);
+		player.getWorld().playEffect(player.getEyeLocation(), Effect.SMOKE, 0);
 		
 		// Kinda hacky, but safe.
 		URLMapRenderer.save();
