a234da639286cd34b64ec03b36eaa76ce899fa29
Nathan Wolf <nathan@WolfBook.local>
2014-01-16 13:48:49 -0800

    Add visual effects to fling and levitate spells. WIP.

--- src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/FlingSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/FlingSpell.java
@@ -1,5 +1,8 @@
 package com.elmakers.mine.bukkit.plugins.magic.spells;
 
+import org.bukkit.Color;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -10,6 +13,9 @@ import org.bukkit.util.Vector;
 import com.elmakers.mine.bukkit.plugins.magic.Spell;
 import com.elmakers.mine.bukkit.plugins.magic.SpellEventType;
 import com.elmakers.mine.bukkit.plugins.magic.SpellResult;
+import com.elmakers.mine.bukkit.utilities.EffectRing;
+import com.elmakers.mine.bukkit.utilities.EffectTrail;
+import com.elmakers.mine.bukkit.utilities.ParticleType;
 import com.elmakers.mine.bukkit.utilities.borrowed.ConfigurationNode;
 
 public class FlingSpell extends Spell
@@ -20,6 +26,12 @@ public class FlingSpell extends Spell
 	protected int defaultMaxSpeedAtElevation = 64;
 	protected double defaultMinMagnitude = 1.5;
 	protected double defaultMaxMagnitude = 6; 
+
+    private final static int effectSpeed = 2;
+    private final static int effectPeriod = 2;
+    private final static int maxEffectRange = 16;
+    private final static int maxRingEffectRange = 4;
+    private final static int ringEffectAmount = 6;
 
 	@Override
 	public SpellResult onCast(ConfigurationNode parameters) 
@@ -62,6 +74,21 @@ public class FlingSpell extends Spell
 			velocity.setY(0);
 		}
 
+		// Visual effect
+		int effectRange = Math.min(getMaxRange(), maxEffectRange / effectSpeed);
+		Location effectLocation = getPlayer().getEyeLocation();
+		Vector effectDirection = effectLocation.getDirection();
+		
+		EffectTrail effect = new EffectTrail(controller.getPlugin(), effectLocation, effectDirection, effectRange);
+		effect.setParticleType(ParticleType.SPELL);
+		effect.setParticleCount(3);
+		Color effectColor = mage.getEffectColor();
+		effect.setEffectData(effectColor != null ? effectColor.asRGB() : 255);
+		effect.setParticleOffset(2.0f, 2.0f, 2.0f);
+		effect.setSpeed(effectSpeed);
+		effect.setPeriod(effectPeriod);
+		effect.start();
+		
 		velocity.multiply(magnitude);
 		getPlayer().setVelocity(velocity);
 		castMessage("Whee!");
@@ -71,6 +98,7 @@ public class FlingSpell extends Spell
 		return SpellResult.SUCCESS;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onPlayerDamage(EntityDamageEvent event)
 	{
@@ -84,6 +112,18 @@ public class FlingSpell extends Spell
 		{
 			event.setCancelled(true);
 			lastFling = 0;
+			
+			// Visual effect
+			Location effectLocation = event.getEntity().getLocation();
+			Block block = event.getEntity().getLocation().getBlock();
+			block = block.getRelative(BlockFace.DOWN);
+			int effectRange = Math.min(getMaxRange(), maxRingEffectRange / effectSpeed);
+			EffectRing effect = new EffectRing(controller.getPlugin(), effectLocation, effectRange, ringEffectAmount);
+			effect.setEffect(Effect.STEP_SOUND);
+			effect.setData(block.getTypeId());
+			effect.setSpeed(effectSpeed);
+			effect.setPeriod(effectPeriod);
+			effect.start();
 		}
 	}
 }
--- src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/LevitateSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magic/spells/LevitateSpell.java
@@ -1,6 +1,11 @@
 package com.elmakers.mine.bukkit.plugins.magic.spells;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.util.Vector;
@@ -8,12 +13,21 @@ import org.bukkit.util.Vector;
 import com.elmakers.mine.bukkit.plugins.magic.Spell;
 import com.elmakers.mine.bukkit.plugins.magic.SpellEventType;
 import com.elmakers.mine.bukkit.plugins.magic.SpellResult;
+import com.elmakers.mine.bukkit.utilities.EffectRing;
+import com.elmakers.mine.bukkit.utilities.EffectTrail;
+import com.elmakers.mine.bukkit.utilities.ParticleType;
 import com.elmakers.mine.bukkit.utilities.borrowed.ConfigurationNode;
 
 public class LevitateSpell extends Spell
 {
 	private long levitateEnded;
 	private final long safetyLength = 10000;
+
+    private final static int effectSpeed = 2;
+    private final static int effectPeriod = 2;
+    private final static int maxEffectRange = 16;
+    private final static int maxRingEffectRange = 4;
+    private final static int ringEffectAmount = 6;
 	
 	@Override
 	public SpellResult onCast(ConfigurationNode parameters) 
@@ -41,6 +55,21 @@ public class LevitateSpell extends Spell
 	
 	@Override
 	public void onActivate() {
+		// Visual effect
+		int effectRange = Math.min(getMaxRange(), maxEffectRange / effectSpeed);
+		Location effectLocation = getPlayer().getEyeLocation();
+		Vector effectDirection = new Vector(0, 1, 0);
+		
+		EffectTrail effect = new EffectTrail(controller.getPlugin(), effectLocation, effectDirection, effectRange);
+		effect.setParticleType(ParticleType.SPELL);
+		effect.setParticleCount(3);
+		Color effectColor = mage.getEffectColor();
+		effect.setEffectData(effectColor != null ? effectColor.asRGB() : 16711935);
+		effect.setParticleOffset(4.0f, 4.0f, 4.0f);
+		effect.setSpeed(effectSpeed);
+		effect.setPeriod(effectPeriod);
+		effect.start();
+				
 		Vector velocity = getPlayer().getVelocity();
 		velocity.setY(velocity.getY() + 2);
 		getPlayer().setVelocity(velocity);
@@ -48,12 +77,14 @@ public class LevitateSpell extends Spell
 			public void run() {
 				getPlayer().setAllowFlight(true);
 				getPlayer().setFlying(true);
+				
 			}
 		}, 2);
 
 		castMessage("You feel lighter");
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onPlayerDamage(EntityDamageEvent event)
 	{
@@ -67,6 +98,18 @@ public class LevitateSpell extends Spell
 		{
 			event.setCancelled(true);
 			levitateEnded = 0;;
+			
+			// Visual effect
+			Location effectLocation = event.getEntity().getLocation();
+			Block block = event.getEntity().getLocation().getBlock();
+			block = block.getRelative(BlockFace.DOWN);
+			int effectRange = Math.min(getMaxRange(), maxRingEffectRange / effectSpeed);
+			EffectRing effect = new EffectRing(controller.getPlugin(), effectLocation, effectRange, ringEffectAmount);
+			effect.setEffect(Effect.STEP_SOUND);
+			effect.setData(block.getTypeId());
+			effect.setSpeed(effectSpeed);
+			effect.setPeriod(effectPeriod);
+			effect.start();
 		}
 	}
 }
