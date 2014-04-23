2c236d1845b69eabb879a4918c5770822abab265
Nathan Wolf <nathan@elmakers.com>
2014-04-23 15:03:34 -0700

    Move the MaterialMapCanvas to the API.

--- src/main/java/com/elmakers/mine/bukkit/block/MaterialBrush.java
+++ src/main/java/com/elmakers/mine/bukkit/block/MaterialBrush.java
@@ -19,7 +19,7 @@ import org.bukkit.util.Vector;
 
 import com.elmakers.mine.bukkit.plugins.magic.Mage;
 import com.elmakers.mine.bukkit.utilities.ConfigurationUtils;
-import com.elmakers.mine.bukkit.utilities.MaterialMapCanvas;
+import com.elmakers.mine.bukkit.utility.MaterialMapCanvas;
 
 public class MaterialBrush extends MaterialAndData {
 	
