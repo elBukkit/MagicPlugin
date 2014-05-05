607ef277f62d580a309c59db5cbaba82e15b5ec6
Nathan Wolf <nathan@elmakers.com>
2014-05-04 22:06:25 -0700

    Remove Mage references from UndoList calls

--- src/main/java/com/elmakers/mine/bukkit/api/block/UndoList.java
+++ src/main/java/com/elmakers/mine/bukkit/api/block/UndoList.java
@@ -11,9 +11,9 @@ import com.elmakers.mine.bukkit.api.magic.Mage;
 public interface UndoList extends BlockList {
 	public void commit();
 	
-	public void scheduleCleanup(Mage mage);
-	public boolean undoScheduled(Mage mage);
-	public boolean undo(Mage mage);
+	public void scheduleCleanup();
+	public boolean undoScheduled();
+	public boolean undo();
 	
 	public void setScheduleUndo(int ttl);
 	public int getScheduledUndo();
