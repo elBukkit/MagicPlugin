d037bd8ece219e39869e2705f643c1895cca8442
Nathan Wolf <nathan@elmakers.com>
2014-05-25 15:14:04 -0700

    Add "op" and "console" parameters to CommandSpell

--- src/main/java/com/elmakers/mine/bukkit/spell/builtin/CommandSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/builtin/CommandSpell.java
@@ -2,10 +2,18 @@ package com.elmakers.mine.bukkit.spell.builtin;
 
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import com.elmakers.mine.bukkit.spell.BaseSpell;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class CommandSpell extends BaseSpell {
+    public final static String[] PARAMETERS = {
+            "command", "console", "op"
+    };
+
     @Override
     public SpellResult onCast(ConfigurationSection parameters)
     {
@@ -14,11 +22,45 @@ public class CommandSpell extends BaseSpell {
             return SpellResult.FAIL;
         }
 
-        CommandSender sender = mage.getCommandSender();
+        boolean asConsole = parameters.getBoolean("console", false);
+        CommandSender sender = asConsole ? Bukkit.getConsoleSender() : mage.getCommandSender();
         if (sender == null) {
             return SpellResult.FAIL;
         }
+
+        boolean opPlayer = parameters.getBoolean("op", false);
+        boolean isOp = sender.isOp();
+        if (opPlayer && !isOp) {
+            sender.setOp(true);
+        }
+
         controller.getPlugin().getServer().dispatchCommand(sender, command);
+
+        if (opPlayer && !isOp) {
+            sender.setOp(false);
+        }
         return SpellResult.CAST;
+    }
+
+    @Override
+    public void getParameters(Collection<String> parameters)
+    {
+        super.getParameters(parameters);
+        parameters.addAll(Arrays.asList(PARAMETERS));
+    }
+
+    @Override
+    public void getParameterOptions(Collection<String> examples, String parameterKey)
+    {
+        super.getParameterOptions(examples, parameterKey);
+
+        if (parameterKey.equals("command")) {
+            examples.add("spawn");
+            examples.add("clear");
+        } else if (parameterKey.equals("console")) {
+            examples.addAll(Arrays.asList(EXAMPLE_BOOLEANS));
+        } else if (parameterKey.equals("op")) {
+            examples.addAll(Arrays.asList(EXAMPLE_BOOLEANS));
+        }
     }
 }
