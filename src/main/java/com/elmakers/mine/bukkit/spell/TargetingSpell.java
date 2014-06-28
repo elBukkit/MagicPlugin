ca28fb05fa2f5eb1428027895bdc98afe561b965
Nathan Wolf <nathan@elmakers.com>
2014-06-27 17:22:01 -0700

    Allow targeting invisible entities
    TODO: configurable?

--- src/main/java/com/elmakers/mine/bukkit/spell/TargetingSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/TargetingSpell.java
@@ -409,7 +409,7 @@ public abstract class TargetingSpell extends BaseSpell {
             // check for Superprotected Mages
             if (isSuperProtected(entity)) continue;
             // Ignore invisible entities
-            if (entity instanceof LivingEntity && ((LivingEntity)entity).hasPotionEffect(PotionEffectType.INVISIBILITY)) continue;
+            // if (entity instanceof LivingEntity && ((LivingEntity)entity).hasPotionEffect(PotionEffectType.INVISIBILITY)) continue;
 
             Target newScore = new Target(getLocation(), entity, maxRange, fov);
             if (newScore.getScore() > 0)
