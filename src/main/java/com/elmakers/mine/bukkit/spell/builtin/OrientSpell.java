0f601cff2ac11e26ed796b87a4a39b109cd615a5
Nathan Wolf <nathan@elmakers.com>
2014-07-15 08:30:08 -0700

    HP: Add Four Point Spell

---
+++ src/main/java/com/elmakers/mine/bukkit/spell/builtin/OrientSpell.java
@@ -0,0 +1,36 @@
+package com.elmakers.mine.bukkit.spell.builtin;
+
+import com.elmakers.mine.bukkit.api.magic.Mage;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.spell.TargetingSpell;
+import com.elmakers.mine.bukkit.utility.Target;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+
+public class OrientSpell extends TargetingSpell
+{
+	@Override
+	public SpellResult onCast(ConfigurationSection parameters) 
+	{
+        Target target = getTarget();
+        Entity entity = target.getEntity();
+        if (entity == null) {
+            return SpellResult.NO_TARGET;
+        }
+
+        if (entity != mage.getEntity() && controller.isMage(entity)) {
+            Mage mage = controller.getMage(entity);
+            if (mage.isSuperProtected()) {
+                return SpellResult.NO_TARGET;
+            }
+        }
+
+        Location location = entity.getLocation();
+        location.setPitch((float)parameters.getDouble("pitch", 0));
+        location.setYaw((float)parameters.getDouble("yaw", 0));
+        entity.teleport(location);
+		
+		return SpellResult.CAST;
+	}
+}
