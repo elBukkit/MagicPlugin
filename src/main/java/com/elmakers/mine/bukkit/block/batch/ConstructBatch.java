bf09c4e079a108869ece0d967e9e1ebbcf39c62b
Nathan Wolf <nathan@elmakers.com>
2014-07-30 15:20:46 -0500

    Fix undo'ing empty schematic blocks

--- src/main/java/com/elmakers/mine/bukkit/block/batch/ConstructBatch.java
+++ src/main/java/com/elmakers/mine/bukkit/block/batch/ConstructBatch.java
@@ -437,7 +437,7 @@ public class ConstructBatch extends BrushBatch {
         Material previousMaterial = block.getType();
         byte previousData = block.getData();
 
-        if (brush.isDifferent(block)) {
+        if (brush.isValid() && brush.isDifferent(block)) {
             registerForUndo(block);
 
             // Check for command overrides
