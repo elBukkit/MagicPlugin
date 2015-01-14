15dfeb226c01ccabbee24ea8dbcf77a9fbe4250c
Nathan Wolf <nathan@elmakers.com>
2015-01-14 14:47:30 -0800

    Fix non action spells using spell actions.

--- src/main/java/com/elmakers/mine/bukkit/spell/BaseSpellAction.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/BaseSpellAction.java
@@ -3,6 +3,7 @@ package com.elmakers.mine.bukkit.spell;
 import com.elmakers.mine.bukkit.api.action.SpellAction;
 import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
+import com.elmakers.mine.bukkit.api.spell.MageSpell;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
 import org.bukkit.configuration.ConfigurationSection;
@@ -11,37 +12,56 @@ import org.bukkit.entity.Entity;
 
 public abstract class BaseSpellAction implements SpellAction
 {
-    protected ActionSpell spell;
+    protected Spell spell;
+    protected MageSpell mageSpell;
+    protected UndoableSpell undoSpell;
     protected ConfigurationSection parameters;
 
     public void registerModified(Entity entity)
     {
-        spell.registerModified(entity);
+        if (undoSpell != null)
+        {
+            undoSpell.registerModified(entity);
+        }
     }
 
-    public void registerForUndo(Entity entity) {
-        spell.registerForUndo(entity);
+    public void registerForUndo(Entity entity)
+    {
+        if (undoSpell != null)
+        {
+            undoSpell.registerForUndo(entity);
+        }
     }
 
     public void registerPotionEffects(Entity entity)
     {
-        spell.registerPotionEffects(entity);
+        if (undoSpell != null)
+        {
+            undoSpell.registerPotionEffects(entity);
+        }
     }
 
-    public Mage getMage() {
-        return spell.getMage();
+    public Mage getMage()
+    {
+        return mageSpell == null ? null : mageSpell.getMage();
     }
 
     public MageController getController() {
-        return spell.getMage().getController();
+        Mage mage = getMage();
+        return mage == null ? null : mage.getController();
     }
 
     @Override
     public void load(Spell spell, ConfigurationSection parameters)
     {
-        if (spell instanceof ActionSpell)
+        this.spell = spell;
+        if (spell instanceof MageSpell)
         {
-            this.spell = (ActionSpell)spell;
+            this.mageSpell = (MageSpell)spell;
+        }
+        if (spell instanceof UndoableSpell)
+        {
+            this.undoSpell = (UndoableSpell)spell;
         }
         this.parameters = parameters;
     }
