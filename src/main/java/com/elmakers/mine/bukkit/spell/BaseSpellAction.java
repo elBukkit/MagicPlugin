86d9508f810f2af5be4fddadb631c7e4e9de5f89
Nathan Wolf <nathan@elmakers.com>
2015-01-15 11:00:27 -0800

    Add more BaseSpellAction passthrough methods, hide spell references.

--- src/main/java/com/elmakers/mine/bukkit/spell/BaseSpellAction.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/BaseSpellAction.java
@@ -1,22 +1,31 @@
 package com.elmakers.mine.bukkit.spell;
 
 import com.elmakers.mine.bukkit.api.action.SpellAction;
+import com.elmakers.mine.bukkit.api.effect.EffectPlayer;
 import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.spell.MageSpell;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Entity;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.util.Vector;
+
+import java.util.Collection;
 
 public abstract class BaseSpellAction implements SpellAction
 {
-    protected BaseSpell spell;
-    protected MageSpell mageSpell;
-    protected UndoableSpell undoSpell;
-    protected ActionSpell actionSpell;
-    protected ConfigurationSection parameters;
+    private Spell spell;
+    private BaseSpell baseSpell;
+    private BlockSpell blockSpell;
+    private MageSpell mageSpell;
+    private UndoableSpell undoSpell;
+    private ActionSpell actionSpell;
+    private ConfigurationSection parameters;
 
     public void registerModified(Entity entity)
     {
@@ -50,6 +59,34 @@ public abstract class BaseSpellAction implements SpellAction
         return null;
     }
 
+    public Vector getDirection() {
+        return baseSpell != null ? baseSpell.getDirection() : null;
+    }
+
+    public Location getLocation() {
+        return baseSpell != null ? baseSpell.getLocation() : null;
+    }
+
+    public Location getEyeLocation() {
+        return baseSpell != null ? baseSpell.getLocation() : null;
+    }
+
+    public boolean isIndestructible(Block block) {
+        return blockSpell != null ? blockSpell.isIndestructible(block) : true;
+    }
+
+    public boolean hasBuildPermission(Block block) {
+        return blockSpell != null ? blockSpell.hasBuildPermission(block) : false;
+    }
+
+    public Collection<EffectPlayer> getEffects(String key) {
+        return spell.getEffects(key);
+    }
+
+    public Collection<PotionEffect> getPotionEffects(ConfigurationSection parameters, Integer duration) {
+        return baseSpell != null ? baseSpell.getPotionEffects(parameters, duration) : null;
+    }
+
     public Mage getMage()
     {
         return mageSpell == null ? null : mageSpell.getMage();
@@ -63,9 +100,10 @@ public abstract class BaseSpellAction implements SpellAction
     @Override
     public void load(Spell spell, ConfigurationSection parameters)
     {
+        this.spell = spell;
         if (spell instanceof BaseSpell)
         {
-            this.spell = (BaseSpell)spell;
+            this.baseSpell = (BaseSpell)spell;
         }
         if (spell instanceof MageSpell)
         {
@@ -78,6 +116,10 @@ public abstract class BaseSpellAction implements SpellAction
         if (spell instanceof ActionSpell)
         {
             this.actionSpell = (ActionSpell)spell;
+        }
+        if (spell instanceof BlockSpell)
+        {
+            this.blockSpell = (BlockSpell)spell;
         }
         this.parameters = parameters;
     }
