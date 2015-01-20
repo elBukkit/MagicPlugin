5339db5408c067e4991c708e180974c655f171ff
Nathan Wolf <nathan@elmakers.com>
2015-01-19 20:29:58 -0800

    Add KillAction (mainly to bypass PVP checks for player killing)

---
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/KillAction.java
@@ -0,0 +1,32 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.api.action.EntityAction;
+import com.elmakers.mine.bukkit.api.magic.Mage;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.spell.BaseSpellAction;
+import com.elmakers.mine.bukkit.utility.CompatibilityUtils;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+
+public class KillAction extends BaseSpellAction implements EntityAction
+{
+	@Override
+	public SpellResult perform(ConfigurationSection parameters, Entity entity)
+	{
+		if (!(entity instanceof LivingEntity))
+		{
+			return SpellResult.NO_TARGET;
+		}
+
+        LivingEntity targetEntity = (LivingEntity)entity;
+		targetEntity.damage(targetEntity.getMaxHealth());
+		return SpellResult.CAST;
+	}
+
+	@Override
+	public boolean isUndoable()
+	{
+		return true;
+	}
+}
