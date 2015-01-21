373985e5611248d34ca5642a821b961da58000b3
Nathan Wolf <nathan@elmakers.com>
2015-01-21 08:35:41 -0800

    Add a "prepare" method that is called on a SpellAction prior to casting.

--- src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
+++ src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
@@ -1,12 +1,14 @@
 package com.elmakers.mine.bukkit.api.action;
 
 import com.elmakers.mine.bukkit.api.spell.Spell;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.util.Collection;
 
 public interface SpellAction
 {
+    public void prepare(ConfigurationSection parameters);
     public void load(Spell spell, ConfigurationSection parameters);
     public ConfigurationSection getParameters(ConfigurationSection baseParameters);
     public void getParameterNames(Collection<String> parameters);
