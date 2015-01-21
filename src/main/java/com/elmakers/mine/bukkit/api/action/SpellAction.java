e83684ead9c666369bb18e5a7e3a6e2a726ca444
Nathan Wolf <nathan@elmakers.com>
2015-01-20 16:15:10 -0800

    Add parameter inspection to actions and ActionSpell

--- src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
+++ src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
@@ -3,10 +3,13 @@ package com.elmakers.mine.bukkit.api.action;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.Collection;
+
 public interface SpellAction
 {
     public void load(Spell spell, ConfigurationSection parameters);
     public ConfigurationSection getParameters(ConfigurationSection baseParameters);
+    public void getParameterNames(Collection<String> parameters);
     public boolean usesBrush();
     public boolean isUndoable();
 }
