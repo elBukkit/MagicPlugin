7b2ce9f5ca48505fd0b03253e95f95ce0b0c4d9c
Nathan Wolf <nathan@elmakers.com>
2015-01-22 13:51:11 -0800

    Add CountAction, used in debugging entity count issues

---
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/CountAction.java
@@ -0,0 +1,59 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.api.action.EntityAction;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.spell.BaseSpellAction;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CountAction extends BaseSpellAction implements EntityAction
+{
+	private Map<String, Integer> counts = new TreeMap<String, Integer>();
+	private int totalCount = 0;
+
+	@Override
+	public SpellResult perform(ConfigurationSection parameters, Entity entity)
+	{
+		String typeString = entity.getType().toString().toLowerCase();
+		Integer count = counts.get(typeString);
+		count = (count == null) ? 1 : count + 1;
+		counts.put(typeString, count);
+		totalCount++;
+
+		return SpellResult.CAST;
+	}
+
+	@Override
+	public boolean isUndoable()
+	{
+		return true;
+	}
+
+
+	@Override
+	public void finish(ConfigurationSection parameters) {
+		CommandSender sender = getMage().getCommandSender();
+		if (sender != null)
+		{
+			sender.sendMessage(ChatColor.DARK_AQUA + "Found " + ChatColor.AQUA + totalCount + ChatColor.DARK_AQUA + " entities in the area");
+			for (Map.Entry<String, Integer> entry : counts.entrySet())
+			{
+				sender.sendMessage(ChatColor.AQUA + entry.getKey() + ChatColor.WHITE + ": " + ChatColor.GOLD + entry.getValue());
+			}
+		}
+
+		totalCount = 0;
+		counts.clear();
+	}
+
+	@Override
+	public void prepare(ConfigurationSection parameters) {
+		totalCount = 0;
+		counts.clear();
+	}
+}
