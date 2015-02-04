80864027b77dd72849a544089e8feb9896f3a2a9
Nathan Wolf <nathan@elmakers.com>
2015-02-04 15:45:58 -0800

    Rename SpellAction.load to initialize, add save/load for data persistence

--- src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
+++ src/main/java/com/elmakers/mine/bukkit/api/action/SpellAction.java
@@ -10,11 +10,13 @@ public interface SpellAction
 {
     public void prepare(ConfigurationSection parameters);
     public void finish(ConfigurationSection parameters);
-    public void load(Spell spell, ConfigurationSection parameters);
+    public void initialize(Spell spell, ConfigurationSection parameters);
     public ConfigurationSection getParameters(ConfigurationSection baseParameters);
     public void getParameterNames(Collection<String> parameters);
     public void getParameterOptions(Collection<String> examples, String parameterKey);
     public boolean usesBrush();
     public boolean isUndoable();
     public String transformMessage(String message);
+    public void load(ConfigurationSection node);
+    public void save(ConfigurationSection node);
 }
