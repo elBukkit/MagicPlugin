65d791d3a1c6d563278ebd5b31d16bb3c2d6a953
Nathan Wolf <nathan@elmakers.com>
2015-02-25 14:35:47 -0800

    Fix an NPE trying to load an empty BrushSpell

--- src/main/java/com/elmakers/mine/bukkit/spell/BrushSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/BrushSpell.java
@@ -92,7 +92,7 @@ public abstract class BrushSpell extends BlockSpell {
     {
         super.loadTemplate(node);
         ConfigurationSection parameters = node.getConfigurationSection("parameters");
-        hasBrush = parameters.contains("brush");
+        hasBrush = parameters != null && parameters.contains("brush");
         usesBrush = node.getBoolean("uses_brush", usesBrush);
     }
 
