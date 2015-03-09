a0b703a60d4750de7ccd2f52c9ea0a2fe6d8190d
Nathan Wolf <nathan@elmakers.com>
2015-03-09 12:52:26 -0700

    Add damage and slow back to deprecated FrostSpell

--- src/main/java/com/elmakers/mine/bukkit/spell/builtin/FrostSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/builtin/FrostSpell.java
@@ -1,7 +1,9 @@
 package com.elmakers.mine.bukkit.spell.builtin;
 
 import com.elmakers.mine.bukkit.action.builtin.CoverAction;
+import com.elmakers.mine.bukkit.action.builtin.DamageAction;
 import com.elmakers.mine.bukkit.action.builtin.FreezeAction;
+import com.elmakers.mine.bukkit.action.builtin.PotionEffectAction;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import com.elmakers.mine.bukkit.action.ActionHandler;
 import com.elmakers.mine.bukkit.spell.BlockSpell;
@@ -29,6 +31,8 @@ public class FrostSpell extends BlockSpell
 
         CoverAction cover = new CoverAction();
         cover.addAction(new FreezeAction());
+        cover.addAction(new DamageAction());
+        cover.addAction(new PotionEffectAction());
         ActionHandler handler = new ActionHandler();
         handler.loadAction(cover);
         registerForUndo();
