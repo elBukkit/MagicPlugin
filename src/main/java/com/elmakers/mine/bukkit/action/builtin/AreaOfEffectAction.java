8332a4dfe0b3bd828c71fe1e2c47d3c31f825e79
Nathan Wolf <nathan@elmakers.com>
2015-03-16 14:48:13 -0700

    Add CompoundEntity base action

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/AreaOfEffectAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/AreaOfEffectAction.java
@@ -1,12 +1,9 @@
 package com.elmakers.mine.bukkit.action.builtin;
 
-import com.elmakers.mine.bukkit.action.ActionHandler;
+import com.elmakers.mine.bukkit.action.CompoundEntityAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
 import com.elmakers.mine.bukkit.api.magic.Mage;
-import com.elmakers.mine.bukkit.api.spell.Spell;
-import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import com.elmakers.mine.bukkit.spell.BaseSpell;
-import com.elmakers.mine.bukkit.action.CompoundAction;
 import com.elmakers.mine.bukkit.utility.CompatibilityUtils;
 import com.elmakers.mine.bukkit.utility.Target;
 import org.bukkit.Location;
@@ -20,21 +17,15 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class AreaOfEffectAction extends CompoundAction
+public class AreaOfEffectAction extends CompoundEntityAction
 {
     private int radius;
-    private boolean targetSelf;
     private int targetCount;
-    private List<WeakReference<Entity>> entities = new ArrayList<WeakReference<Entity>>();
-    private int currentEntity = 0;
-    private CastContext actionContext;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters)
     {
-        super.prepare(context, parameters);
         radius = parameters.getInt("radius", 8);
-        targetSelf = parameters.getBoolean("target_self", false);
         targetCount = parameters.getInt("target_count", -1);
 
         Mage mage = context.getMage();
@@ -43,7 +34,12 @@ public class AreaOfEffectAction extends CompoundAction
             radius = (int)(mage.getRadiusMultiplier() * radius);
         }
 
-        entities.clear();
+        super.prepare(context, parameters);
+    }
+
+    @Override
+    public void prepareEntities(CastContext context, ConfigurationSection parameters, List<WeakReference<Entity>> entities)
+    {
         Entity sourceEntity = context.getEntity();
         Location sourceLocation = context.getTargetLocation();
         List<Entity> candidates = CompatibilityUtils.getNearbyEntities(sourceLocation, radius, radius, radius);
@@ -74,59 +70,18 @@ public class AreaOfEffectAction extends CompoundAction
                 }
             }
         }
-
     }
-
-    @Override
-    public void reset(CastContext context)
-    {
-        super.reset(context);
-        actionContext = createContext(context);
-        currentEntity = 0;
-    }
-
-	@Override
-    public SpellResult perform(CastContext context)
-	{
-        SpellResult result = SpellResult.NO_TARGET;
-        while (currentEntity < entities.size())
-        {
-            Entity entity = entities.get(currentEntity).get();
-            if (entity == null)
-            {
-                skippedActions(context);
-                continue;
-            }
-            actionContext.setTargetEntity(entity);
-            actionContext.setTargetLocation(entity.getLocation());
-            SpellResult entityResult = performActions(actionContext);
-            result = result.min(entityResult);
-            if (entityResult == SpellResult.PENDING) {
-                break;
-            }
-            currentEntity++;
-            if (currentEntity < entities.size())
-            {
-                super.reset(context);
-            }
-        }
-
-		return result;
-	}
 
     @Override
     public void getParameterNames(Collection<String> parameters) {
         super.getParameterNames(parameters);
         parameters.add("radius");
         parameters.add("target_count");
-        parameters.add("target_self");
     }
 
     @Override
     public void getParameterOptions(Collection<String> examples, String parameterKey) {
-        if (parameterKey.equals("target_self")) {
-            examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_BOOLEANS)));
-        } else if (parameterKey.equals("target_count") || parameterKey.equals("radius")) {
+        if (parameterKey.equals("target_count") || parameterKey.equals("radius")) {
             examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_SIZES)));
         } else {
             super.getParameterOptions(examples, parameterKey);
@@ -136,15 +91,5 @@ public class AreaOfEffectAction extends CompoundAction
     @Override
     public boolean requiresTarget() {
         return true;
-    }
-
-    @Override
-    public Object clone()
-    {
-        AreaOfEffectAction action = (AreaOfEffectAction)super.clone();
-        if (action != null) {
-            action.entities = new ArrayList<WeakReference<Entity>>(this.entities);
-        }
-        return action;
     }
 }
