0cae0bffef8dc370a4768b432c569cac5c986c16
Nathan Wolf <nathan@elmakers.com>
2015-03-18 10:02:37 -0700

    Add backfire and reflect to ModifyBlock

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/ModifyBlockAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/ModifyBlockAction.java
@@ -4,6 +4,7 @@ import com.elmakers.mine.bukkit.action.BaseSpellAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
 import com.elmakers.mine.bukkit.api.block.MaterialBrush;
 import com.elmakers.mine.bukkit.api.magic.Mage;
+import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import com.elmakers.mine.bukkit.block.MaterialAndData;
 import com.elmakers.mine.bukkit.spell.BaseSpell;
@@ -13,6 +14,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.FallingBlock;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.util.Vector;
 
 import java.util.Arrays;
@@ -26,11 +28,17 @@ public class ModifyBlockAction extends BaseSpellAction {
     private boolean spawnFallingBlocks;
     private double fallingBlockSpeed;
     private Vector fallingBlockDirection;
+    private int breakable = 0;
+    private double backfireChance = 0;
+    private boolean applyPhysics = false;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
         spawnFallingBlocks = parameters.getBoolean("falling", false);
+        applyPhysics = parameters.getBoolean("physics", false);
+        breakable = parameters.getInt("breakable", 0);
+        backfireChance = parameters.getDouble("reflect_chance", 0);
         if (spawnFallingBlocks && parameters.contains("direction"))
         {
             fallingBlockSpeed = parameters.getDouble("speed", 1.0);
@@ -77,12 +85,13 @@ public class ModifyBlockAction extends BaseSpellAction {
         if (replaceable == null || replaceable.contains(new MaterialAndData(block))) {
             Material previousMaterial = block.getType();
             byte previousData = block.getData();
+            MageController controller = context.getController();
 
             if (brush.isDifferent(block)) {
                 context.registerForUndo(block);
                 Mage mage = context.getMage();
                 brush.update(mage, block.getLocation());
-                brush.modify(block);
+                brush.modify(block, applyPhysics);
 
                 if (spawnFallingBlocks)
                 {
@@ -102,6 +111,13 @@ public class ModifyBlockAction extends BaseSpellAction {
                     }
                 }
             }
+
+            if (breakable > 0) {
+                block.setMetadata("breakable", new FixedMetadataValue(controller.getPlugin(), breakable));
+            }
+            if (backfireChance > 0) {
+                block.setMetadata("backfire", new FixedMetadataValue(controller.getPlugin(), backfireChance));
+            }
             return SpellResult.CAST;
         }
 
@@ -113,17 +129,22 @@ public class ModifyBlockAction extends BaseSpellAction {
         super.getParameterNames(parameters);
         parameters.add("falling");
         parameters.add("speed");
-        parameters.add("falling_direction");
+        parameters.add("direction");
+        parameters.add("reflect_chance");
+        parameters.add("breakable");
+        parameters.add("physics");
     }
 
     @Override
     public void getParameterOptions(Collection<String> examples, String parameterKey) {
-        if (parameterKey.equals("falling")) {
+        if (parameterKey.equals("falling") || parameterKey.equals("physics")) {
             examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_BOOLEANS)));
-        } else if (parameterKey.equals("speed")) {
+        } else if (parameterKey.equals("speed") || parameterKey.equals("breakable")) {
             examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_SIZES)));
-        } else if (parameterKey.equals("falling_direction")) {
+        } else if (parameterKey.equals("direction")) {
             examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_VECTOR_COMPONENTS)));
+        } else if (parameterKey.equals("reflect_chance")) {
+            examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_PERCENTAGES)));
         } else {
             super.getParameterOptions(examples, parameterKey);
         }
