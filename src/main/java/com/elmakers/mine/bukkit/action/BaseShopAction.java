b0326aaff208792b5c57ea3dce914879bcb70ed4
Nathan Wolf <nathan@elmakers.com>
2015-04-04 18:58:26 -0700

    Add options to base shop to require wands and paths

--- src/main/java/com/elmakers/mine/bukkit/action/BaseShopAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/BaseShopAction.java
@@ -6,7 +6,10 @@ import com.elmakers.mine.bukkit.api.block.CurrencyItem;
 import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.magic.Messages;
+import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.api.wand.Wand;
+import com.elmakers.mine.bukkit.api.wand.WandUpgradePath;
 import com.elmakers.mine.bukkit.block.MaterialAndData;
 import com.elmakers.mine.bukkit.integration.VaultController;
 import com.elmakers.mine.bukkit.spell.BaseSpell;
@@ -31,12 +34,17 @@ import java.util.Map;
 
 public abstract class BaseShopAction extends BaseSpellAction implements GUIAction
 {
-    private boolean useXP = false;
-    private boolean sell = false;
-    private boolean useItems = false;
-    private boolean showConfirmation = true;
-    private MaterialAndData confirmFillMaterial;
-    private CastContext context;
+    protected boolean requireWand = false;
+    private String requiredPath = null;
+    private String requiresCompletedPath = null;
+    private String exactPath = null;
+    protected boolean autoUpgrade = false;
+    protected boolean useXP = false;
+    protected boolean sell = false;
+    protected boolean useItems = false;
+    protected boolean showConfirmation = true;
+    protected MaterialAndData confirmFillMaterial;
+    protected CastContext context;
     private Map<Integer, ShopItem> showingItems;
 
     protected class ShopItem {
@@ -62,6 +70,56 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
 
     }
 
+    public SpellResult checkContext(CastContext context) {
+        Mage mage = context.getMage();
+        MageController controller = mage.getController();
+        Player player = mage.getPlayer();
+        if (player == null) {
+            return SpellResult.PLAYER_REQUIRED;
+        }
+        if (!requireWand) {
+            return SpellResult.CAST;
+        }
+        Wand wand = mage.getActiveWand();
+        if (wand == null) {
+            context.sendMessage("no_wand");
+            return SpellResult.FAIL;
+        }
+
+        WandUpgradePath path = wand.getPath();
+
+        // Check path requirements
+        if (requiredPath != null || exactPath != null) {
+            if (path == null) {
+                context.sendMessage(context.getMessage("no_path").replace("$wand", wand.getName()));
+                return SpellResult.FAIL;
+            }
+
+            if ((requiredPath != null && !path.hasPath(requiredPath)) || (exactPath != null && !exactPath.equals(path.getKey()))) {
+                WandUpgradePath requiresPath = controller.getPath(requiredPath);
+                if (requiresPath != null) {
+                    context.sendMessage(context.getMessage("no_required_path").replace("$path", requiresPath.getName()));
+                } else {
+                    context.getLogger().warning("Invalid path specified in AddSpell action: " + requiredPath);
+                }
+                return SpellResult.FAIL;
+            }
+            if (requiresCompletedPath != null) {
+                WandUpgradePath pathUpgrade = path.getUpgrade();
+                if (pathUpgrade == null) {
+                    context.sendMessage(context.getMessage("no_upgrade").replace("$wand", wand.getName()));
+                    return SpellResult.FAIL;
+                }
+                if (path.canEnchant(wand)) {
+                    context.sendMessage(context.getMessage("no_path_end").replace("$path", pathUpgrade.getName()));
+                    return SpellResult.FAIL;
+                }
+            }
+        }
+
+        return SpellResult.CAST;
+    }
+
     @Override
     public void clicked(InventoryClickEvent event)
     {
@@ -75,6 +133,7 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
         MageController controller = context.getController();
         Messages messages = controller.getMessages();
         Mage mage = context.getMage();
+        Wand wand = mage.getActiveWand();
         boolean isItems = (useItems || !VaultController.hasEconomy()) && controller.getWorthItem() != null;
         boolean isXP = (useXP || (!VaultController.hasEconomy() && controller.getWorthItem() == null));
         ShopItem shopItem = showingItems.get(slotIndex);
@@ -191,6 +250,19 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
             }
             else
             {
+                if (requireWand) {
+                    if (wand == null) {
+                        context.sendMessage("no_wand");
+                        mage.deactivateGUI();
+                        return;
+                    }
+                    if (!wand.addItem(item)) {
+                        String inapplicable = context.getMessage("not_applicable", "You can't buy $item").replace("$item", itemName);
+                        context.sendMessage(inapplicable);
+                        mage.deactivateGUI();
+                        return;
+                    }
+                }
                 if (isXP) {
                     mage.removeExperience((int)(double)worth);
                 }
@@ -203,7 +275,17 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
                     VaultController.getInstance().withdrawPlayer(mage.getPlayer(), worth);
                 }
 
-                context.getController().giveItemToPlayer(mage.getPlayer(), item);
+                if (!requireWand) {
+                    context.getController().giveItemToPlayer(mage.getPlayer(), item);
+                }
+
+                if (wand != null && autoUpgrade) {
+                    com.elmakers.mine.bukkit.api.wand.WandUpgradePath path = wand.getPath();
+                    WandUpgradePath nextPath = path != null ? path.getUpgrade(): null;
+                    if (nextPath != null && path.checkUpgradeRequirements(wand, null) && !path.canEnchant(wand)) {
+                        path.upgrade(wand, mage);
+                    }
+                }
             }
             onPurchase(context);
         }
@@ -221,6 +303,17 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
         useItems = parameters.getBoolean("use_items", false);
         showConfirmation = parameters.getBoolean("confirm", true);
         confirmFillMaterial = ConfigurationUtils.getMaterialAndData(parameters, "confirm_filler", new MaterialAndData(Material.AIR));
+        requiredPath = parameters.getString("path", null);
+        exactPath = parameters.getString("path_exact", null);
+        requiresCompletedPath = parameters.getString("path_end", null);
+        autoUpgrade = parameters.getBoolean("auto_upgrade", false);
+        requireWand = parameters.getBoolean("require_wand", false);
+        if (requiresCompletedPath != null) {
+            requiredPath = requiresCompletedPath;
+        }
+        if (requiredPath != null || exactPath != null) {
+            requireWand = true;
+        }
     }
 
     public SpellResult showItems(CastContext context, List<ShopItem> items) {
@@ -377,19 +470,27 @@ public abstract class BaseShopAction extends BaseSpellAction implements GUIActio
     }
 
     @Override
-    public void getParameterNames(Collection<String> parameters)
+    public void getParameterNames(Spell spell, Collection<String> parameters)
     {
-        super.getParameterNames(parameters);
+        super.getParameterNames(spell, parameters);
         parameters.add("confirm");
+        parameters.add("path");
+        parameters.add("path_end");
+        parameters.add("path_exact");
+        parameters.add("auto_upgrade");
+        parameters.add("require_wand");
     }
 
     @Override
-    public void getParameterOptions(Collection<String> examples, String parameterKey)
+    public void getParameterOptions(Spell spell, String parameterKey, Collection<String> examples)
     {
-        if (parameterKey.equals("confirm")) {
+        MageController controller = spell.getController();
+        if (parameterKey.equals("path") || parameterKey.equals("path_exact") || parameterKey.equals("path_end")) {
+            examples.addAll(controller.getWandPathKeys());
+        } else if (parameterKey.equals("require_wand") || parameterKey.equals("confirm") || parameterKey.equals("auto_upgrade")) {
             examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_BOOLEANS)));
         } else {
-            super.getParameterOptions(examples, parameterKey);
+            super.getParameterOptions(spell, parameterKey, examples);
         }
     }
 }
