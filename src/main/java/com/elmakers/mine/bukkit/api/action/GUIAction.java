cf65cc5cd323af23dd9fc9be69a3df9bc96b1855
Nathan Wolf <nathan@elmakers.com>
2015-04-20 15:15:59 -0700

    GUIActions don't need to be full SpellActions

--- src/main/java/com/elmakers/mine/bukkit/api/action/GUIAction.java
+++ src/main/java/com/elmakers/mine/bukkit/api/action/GUIAction.java
@@ -4,7 +4,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 
-public interface GUIAction extends SpellAction {
+public interface GUIAction {
     public void deactivated();
     public void clicked(InventoryClickEvent event);
     public void dragged(InventoryDragEvent event);
