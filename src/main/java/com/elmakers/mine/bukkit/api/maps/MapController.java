fa91c7057bb1de32635ca3e905e8323bab17b1b1
Nathan Wolf <nathan@elmakers.com>
2015-04-29 09:32:34 -0700

    Add some additional methods to the MapController API

--- src/main/java/com/elmakers/mine/bukkit/api/maps/MapController.java
+++ src/main/java/com/elmakers/mine/bukkit/api/maps/MapController.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface MapController {
     public Collection<URLMap> getAll();
+    public void loadMap(String world, short id, String url, String name, int x, int y, int width, int height, Integer priority);
     public ItemStack getURLItem(String world, String url, String name, int x, int y, int width, int height, Integer priority);
     public void forceReloadPlayerPortrait(String worldName, String playerName);
     public ItemStack getPlayerPortrait(String worldName, String playerName, Integer priority, String photoName);
+    public ItemStack getMapItem(short id);
+    public boolean hasMap(short id);
 }
