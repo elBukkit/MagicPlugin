9ed1d50de0d3d4f95c580dae9b5aa7a618b4e6c0
Nathan Wolf <nathan@elmakers.com>
2015-10-23 09:50:37 -0700

    ConeOfEffect uses Targeting class

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/ConeOfEffectAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/ConeOfEffectAction.java
@@ -2,6 +2,9 @@ package com.elmakers.mine.bukkit.action.builtin;
 
 import com.elmakers.mine.bukkit.action.CompoundEntityAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.Spell;
+import com.elmakers.mine.bukkit.api.spell.TargetType;
+import com.elmakers.mine.bukkit.utility.Targeting;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 
@@ -11,11 +14,20 @@ import java.util.List;
 public class ConeOfEffectAction extends CompoundEntityAction
 {
     private int targetCount;
+    private double range;
+    private Targeting targeting;
+
+    @Override
+    public void initialize(Spell spell, ConfigurationSection baseParameters) {
+        super.initialize(spell, baseParameters);
+        targeting = new Targeting();
+    }
 
     @Override
     public void reset(CastContext context) {
         super.reset(context);
         createActionContext(context);
+        targeting.start(context.getEyeLocation());
     }
 
     @Override
@@ -23,10 +35,25 @@ public class ConeOfEffectAction extends CompoundEntityAction
     {
         super.prepare(context, parameters);
         targetCount = parameters.getInt("target_count", -1);
+        range = parameters.getDouble("range", 16);
+        range = range * context.getMage().getRangeMultiplier();
+
+        targeting.processParameters(parameters);
+
+        // Some parameter tweaks to make sure things are sane
+        TargetType targetType = targeting.getTargetType();
+        if (targetType == TargetType.NONE || targetType == TargetType.BLOCK) {
+            targeting.setTargetType(TargetType.OTHER);
+        } else if (targetType == TargetType.SELF) {
+            targeting.setTargetType(TargetType.ANY);
+        }
+
+        // COE never uses hitbox, there's the Retarget action for that.
+        targeting.setUseHitbox(false);
     }
 
     @Override
     public void addEntities(CastContext context, List<WeakReference<Entity>> entities) {
-        context.getTargetEntities(targetCount, entities);
+        targeting.getTargetEntities(context, range, targetCount, entities);
     }
 }
