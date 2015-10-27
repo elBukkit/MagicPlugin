20bc613de104bb8b85fd10ede079aca6ee2b8231
Nathan Wolf <nathan@elmakers.com>
2015-10-27 14:13:12 -0700

    Add "spells" parameter to Cancel action

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/CancelAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/CancelAction.java
@@ -6,19 +6,31 @@ import com.elmakers.mine.bukkit.api.batch.Batch;
 import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class CancelAction extends BaseSpellAction
 {
 	private String undoListName;
-    private String targetSpellKey;
+    private Collection<String> spellKeys;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters)
     {
         super.prepare(context, parameters);
-        targetSpellKey = parameters.getString("spell", null);
+        if (parameters.contains("spells")) {
+            spellKeys = ConfigurationUtils.getStringList(parameters, "spells");
+        } else if (parameters.contains("spell")) {
+            String targetSpellKey = parameters.getString("spell", null);
+            if (targetSpellKey != null && !targetSpellKey.isEmpty()) {
+                spellKeys = new ArrayList<String>();
+                spellKeys.add(targetSpellKey);
+            }
+        }
     }
 
     @Override
@@ -30,16 +42,24 @@ public class CancelAction extends BaseSpellAction
             return SpellResult.NO_TARGET;
         }
 
+        SpellResult result = SpellResult.NO_TARGET;
         Mage targetMage = controller.getMage(targetEntity);
-        Batch batch = (targetSpellKey == null || targetSpellKey.isEmpty())
-                ? targetMage.cancelPending(true)
-                : targetMage.cancelPending(targetSpellKey);
-        if (batch == null) {
-            return SpellResult.NO_TARGET;
+        if (spellKeys == null) {
+            Batch batch = targetMage.cancelPending(true);
+            if (batch != null) {
+                result = SpellResult.CAST;
+                undoListName = batch.getName();
+            }
+        } else {
+            for (String spellKey : spellKeys) {
+                Batch batch = targetMage.cancelPending(spellKey);
+                if (batch != null) {
+                    result = SpellResult.CAST;
+                    undoListName = batch.getName();
+                }
+            }
         }
-        undoListName = batch.getName();
-
-        return SpellResult.CAST;
+        return result;
 	}
 
     @Override
@@ -49,6 +69,6 @@ public class CancelAction extends BaseSpellAction
 
     @Override
     public String transformMessage(String message) {
-		return message.replace("$spell", undoListName == null ? "Unknown" : undoListName);
+		return message.replace("$undo", undoListName == null ? "Unknown" : undoListName);
 	}
 }
