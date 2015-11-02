d6ec6b70dc498313992c5efa881ff65e0a552d39
Nathan Wolf <nathan@elmakers.com>
2015-11-02 13:00:33 -0800

    Fix Spell Shop

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/SpellShopAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/SpellShopAction.java
@@ -51,6 +51,7 @@ public class SpellShopAction extends BaseShopAction
         showRequired = parameters.getBoolean("show_required", false);
         showFree = parameters.getBoolean("show_free", false);
         requireWand = true;
+        applyToWand = true;
     }
 
     @Override
