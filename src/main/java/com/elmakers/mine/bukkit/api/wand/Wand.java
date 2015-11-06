cecd3a46469994f21b4800ff188898698a491f39
Nathan Wolf <nathan@elmakers.com>
2015-11-05 17:06:14 -0800

    Add Wand.isUndroppable

--- src/main/java/com/elmakers/mine/bukkit/api/wand/Wand.java
+++ src/main/java/com/elmakers/mine/bukkit/api/wand/Wand.java
@@ -102,6 +102,7 @@ public interface Wand extends CostReducer {
     public boolean cast();
     public boolean isBound();
     public boolean isSoul();
+    public boolean isUndroppable();
     public boolean isQuickCastDisabled();
     public void damageDealt(double damage, Entity target);
     public int getSpellLevel(String spellKey);
