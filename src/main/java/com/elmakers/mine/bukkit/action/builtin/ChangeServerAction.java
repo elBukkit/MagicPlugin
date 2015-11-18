971a26e0e3ca5e2dd10775bf0b9a4bbd8c4a149d
Nathan Wolf <nathan@elmakers.com>
2015-11-18 12:01:42 -0800

    Add ChangeServer action

---
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/ChangeServerAction.java
@@ -0,0 +1,41 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.BaseSpellAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class ChangeServerAction extends BaseSpellAction
+{
+    private String serverName;
+
+    @Override
+    public void prepare(CastContext context, ConfigurationSection parameters)
+    {
+        super.prepare(context, parameters);
+        serverName = parameters.getString("server", "");
+    }
+
+	@Override
+	public SpellResult perform(CastContext context)
+	{
+        Entity targetEntity = context.getTargetEntity();
+		if (!(targetEntity instanceof Player))
+		{
+			return SpellResult.NO_TARGET;
+		}
+
+        Player player = (Player)targetEntity;
+        context.getController().sendPlayerToServer(player, serverName);
+
+		return SpellResult.CAST;
+	}
+
+    @Override
+    public boolean requiresTargetEntity()
+    {
+        return true;
+    }
+}
