b6ba70fa691faa29db348dedf5a0b82e07e9ef17
Nathan Wolf <nathan@elmakers.com>
2015-11-19 08:59:10 -0800

    Add Multiply action

---
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/MultiplyAction.java
@@ -0,0 +1,81 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.ActionHandler;
+import com.elmakers.mine.bukkit.action.CompoundAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyAction extends CompoundAction
+{
+    private List<ActionHandler> remaining = null;
+    private List<ActionHandler> multiplied = null;
+    private int multiply;
+
+    public void prepare(CastContext context, ConfigurationSection parameters) {
+        super.prepare(context, parameters);
+        multiply = ConfigurationUtils.getInteger(parameters, "multiply", 2);
+        multiplied = new ArrayList<ActionHandler>();
+
+        ActionHandler base = addHandler("actions");
+        if (base != null)
+        {
+            ConfigurationSection initialParameters = parameters.getConfigurationSection("first");
+            if (initialParameters != null)
+            {
+                ConfigurationSection combined = ConfigurationUtils.addConfigurations(new MemoryConfiguration(), parameters);
+                initialParameters = ConfigurationUtils.addConfigurations(combined, initialParameters);
+            }
+            for (int i = 0; i < multiply; i++)
+            {
+                ActionHandler handler = (ActionHandler)base.clone();
+                if (i == 0 && initialParameters != null)
+                {
+                    handler.prepare(context, initialParameters);
+                }
+                else
+                {
+                    handler.prepare(context, parameters);
+                }
+                multiplied.add(handler);
+            }
+        }
+    }
+
+    @Override
+    public void reset(CastContext context) {
+        super.reset(context);
+        remaining = new ArrayList<ActionHandler>(multiplied);
+        for (ActionHandler handler : remaining) {
+            handler.reset(context);
+        }
+    }
+
+    @Override
+	public SpellResult perform(CastContext context) {
+        SpellResult result = SpellResult.NO_ACTION;
+        if (remaining.size() == 0) return result;
+
+        int startingWork = context.getWorkAllowed();
+        List<ActionHandler> subActions = new ArrayList<ActionHandler>(remaining);
+        remaining.clear();
+        context.setWorkAllowed(0);
+        int splitWork = Math.max(1, startingWork / subActions.size());
+        for (ActionHandler action : subActions) {
+            context.setWorkAllowed(context.getWorkAllowed() + splitWork);
+            SpellResult actionResult = action.perform(context);
+            context.addResult(actionResult);
+            if (actionResult.isStop()) {
+                remaining.add(action);
+            }
+            result = result.min(actionResult);
+        }
+
+		return result;
+	}
+}
