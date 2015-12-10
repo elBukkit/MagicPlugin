a13a52f3b2b6c2f2fc8da986191f25a729efb46b
Nathan Wolf <nathan@elmakers.com>
2015-12-10 11:43:08 -0800

    Add Stop action, for cancelling a spell at a certain point in its action list

---
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/StopAction.java
@@ -0,0 +1,14 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.BaseSpellAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+
+public class StopAction extends BaseSpellAction
+{
+	@Override
+	public SpellResult perform(CastContext context)
+	{
+        return SpellResult.CANCELLED;
+	}
+}
