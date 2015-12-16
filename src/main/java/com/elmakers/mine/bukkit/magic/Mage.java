6c50feb94f3c41d1f25624545ddf56365d0464b9
Nathan Wolf <nathan@elmakers.com>
2015-12-16 10:20:51 -0800

    Mage.save() returns false if the Mage is still loading

--- src/main/java/com/elmakers/mine/bukkit/magic/Mage.java
+++ src/main/java/com/elmakers/mine/bukkit/magic/Mage.java
@@ -700,6 +700,7 @@ public class Mage implements CostReducer, com.elmakers.mine.bukkit.api.magic.Mag
 
     @Override
     public boolean save(MageData data) {
+        if (loading) return false;
         try {
             data.setName(getName());
             data.setId(getId());
