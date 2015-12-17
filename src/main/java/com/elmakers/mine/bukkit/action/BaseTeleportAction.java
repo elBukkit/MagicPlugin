d29f7af4bb999b0610d084add5d3c9510574770c
Nathan Wolf <nathan@elmakers.com>
2015-12-17 14:16:37 -0800

    Move "safe" flag to BaseTeleportAction

--- src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
@@ -1,15 +1,23 @@
 package com.elmakers.mine.bukkit.action;
 
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
 
 public abstract class BaseTeleportAction extends BaseSpellAction
 {
     protected int verticalSearchDistance;
+    protected boolean safe = true;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
         verticalSearchDistance = parameters.getInt("vertical_range", context.getVerticalSearchDistance());
+        safe = parameters.getBoolean("safe", true);
+    }
+
+    protected void teleport(CastContext context, Entity entity, Location targetLocation) {
+        context.teleport(entity, targetLocation, verticalSearchDistance, safe);
     }
 }
