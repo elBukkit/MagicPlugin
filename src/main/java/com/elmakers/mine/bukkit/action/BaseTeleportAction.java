4e1e46cb989f56f8d69375f33cabbf565200fcdb
Nathan Wolf <nathan@elmakers.com>
2016-01-27 12:03:35 -0800

    Add require_build parameter to teleport actions

--- src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
@@ -1,6 +1,7 @@
 package com.elmakers.mine.bukkit.action;
 
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
@@ -9,15 +10,20 @@ public abstract class BaseTeleportAction extends BaseSpellAction
 {
     protected int verticalSearchDistance;
     protected boolean safe = true;
+    private boolean requiresBuildPermission = false;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
         verticalSearchDistance = parameters.getInt("vertical_range", context.getVerticalSearchDistance());
         safe = parameters.getBoolean("safe", true);
+        requiresBuildPermission = parameters.getBoolean("require_build", false);
     }
 
-    protected void teleport(CastContext context, Entity entity, Location targetLocation) {
-        context.teleport(entity, targetLocation, verticalSearchDistance, safe);
+    protected SpellResult teleport(CastContext context, Entity entity, Location targetLocation) {
+        if (requiresBuildPermission && !context.hasBuildPermission(targetLocation.getBlock())) {
+            return SpellResult.INSUFFICIENT_PERMISSION;
+        }
+        return context.teleport(entity, targetLocation, verticalSearchDistance, safe) ? SpellResult.CAST : SpellResult.FAIL;
     }
 }
