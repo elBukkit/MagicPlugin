ea2242cff7e424a63681e12e559caaac6734b368
Jeremy Wood <farachan@gmail.com>
2016-01-30 15:48:39 -0500

    Added conversations to CommandAction to get arg input from players.

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/CommandAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/CommandAction.java
@@ -6,18 +6,18 @@ import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.magic.MagicPlugin;
 import com.elmakers.mine.bukkit.spell.BaseSpell;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.conversations.*;
 import org.bukkit.entity.Entity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 
 /**
@@ -42,21 +42,31 @@ import java.util.logging.Level;
  * @t - target mage name
  * @tuuid - target entity uuid
  * @tworld, @tx, @ty, @tz - target location
+ *
+ * If @arg is present in the command string a conversation will be created with the player to gather the input.
  */
 public class CommandAction extends BaseSpellAction {
     public final static String[] PARAMETERS = {
-            "command", "console", "op"
+            "command", "console", "op", "local_echo", "modal", "timeout", "escape_sequence"
     };
 
     private List<String> commands = new ArrayList<String>();
     private boolean asConsole;
     private boolean opPlayer;
+    private boolean localEcho;
+    private boolean modal;
+    private int timeout;
+    private String escapeSequence;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
         asConsole = parameters.getBoolean("console", false);
         opPlayer = parameters.getBoolean("op", false);
+        localEcho = parameters.getBoolean("local_echo", true);
+        modal = parameters.getBoolean("modal", false);
+        timeout = parameters.getInt("timeout", 0);
+        escapeSequence = parameters.getString("escape_sequence", "");
     }
 
     @Override
@@ -86,6 +96,7 @@ public class CommandAction extends BaseSpellAction {
         if (sender == null) {
             return SpellResult.FAIL;
         }
+        Queue<String> conversationCommands = new ArrayDeque<String>(commands.size());
         boolean isOp = sender.isOp();
         if (opPlayer && !isOp) {
             sender.setOp(true);
@@ -93,13 +104,20 @@ public class CommandAction extends BaseSpellAction {
         for (String command : commands) {
             try {
                 String converted = context.parameterize(command);
-                controller.getPlugin().getServer().dispatchCommand(sender, converted);
+                if (converted.contains("@arg")) {
+                    conversationCommands.add(converted);
+                } else {
+                    controller.getPlugin().getServer().dispatchCommand(sender, converted);
+                }
             } catch (Exception ex) {
                 controller.getLogger().log(Level.WARNING, "Error running command: " + command, ex);
             }
         }
         if (opPlayer && !isOp) {
             sender.setOp(false);
+        }
+        if (!conversationCommands.isEmpty()) {
+            runConversations(context, conversationCommands, opPlayer);
         }
         return SpellResult.CAST;
     }
@@ -117,10 +135,95 @@ public class CommandAction extends BaseSpellAction {
         if (parameterKey.equals("command")) {
             examples.add("spawn");
             examples.add("clear");
-        } else if (parameterKey.equals("op") || parameterKey.equals("console")) {
+        } else if (parameterKey.equals("escape_sequence")) {
+            examples.add("$$");
+            examples.add("!");
+            examples.add("cancel");
+        } else if (parameterKey.equals("timeout")) {
+            examples.add("5");
+            examples.add("10");
+        } else if (parameterKey.equals("op") || parameterKey.equals("console")
+                || parameterKey.equals("local_echo") || parameterKey.contains("modal")) {
             examples.addAll(Arrays.asList(BaseSpell.EXAMPLE_BOOLEANS));
         } else {
             super.getParameterOptions(spell, parameterKey, examples);
+        }
+    }
+
+    private void runConversations(CastContext context, Queue<String> commands, boolean opPlayer) {
+        CommandSender sender = context.getMage().getCommandSender();
+        if (!(sender instanceof Conversable)) {
+            return;
+        }
+        Conversable conversable = (Conversable) sender;
+        ArgumentsPrompt argumentsPrompt = new ArgumentsPrompt(context, sender, commands, opPlayer);
+        ConversationFactory factory = new ConversationFactory(context.getController().getPlugin());
+        factory.withLocalEcho(localEcho).withModality(modal).withFirstPrompt(argumentsPrompt);
+        if (timeout > 0) {
+            factory.withTimeout(timeout);
+        }
+        if (!escapeSequence.isEmpty()) {
+            factory.withEscapeSequence(escapeSequence);
+        }
+        conversable.beginConversation(factory.buildConversation(conversable));
+    }
+
+    private static class ArgumentsPrompt implements Prompt {
+        private final CastContext context;
+        private final CommandSender sender;
+        private final String currentCommand;
+        private final int argCount;
+        private final String[] arguments;
+        private final Queue<String> remainingCommands;
+        private final boolean opPlayer;
+        private Integer currentArg = 1;
+
+        public ArgumentsPrompt(CastContext context, CommandSender sender, Queue<String> remainingCommands, boolean opPlayer) {
+            this.context = context;
+            this.sender = sender;
+            this.currentCommand = remainingCommands.poll();
+            this.argCount = StringUtils.countMatches(currentCommand, "@arg");
+            this.arguments = new String[argCount];
+            this.remainingCommands = remainingCommands;
+            this.opPlayer = opPlayer;
+        }
+
+        @Override
+        public String getPromptText(ConversationContext conversationContext) {
+            return context.getMessage("command_prompt", "Input argument $argnum of /$command")
+                    .replace("$argnum", currentArg.toString()).replace("$command", currentCommand);
+        }
+
+        @Override
+        public boolean blocksForInput(ConversationContext conversationContext) {
+            return true;
+        }
+
+        @Override
+        public Prompt acceptInput(ConversationContext conversationContext, String s) {
+            arguments[currentArg - 1] = s;
+            currentArg++;
+            if (currentArg > argCount) {
+                runCommand(StringUtils.replaceEach(currentCommand, new String[]{"@arg"}, getArguments()));
+                if (!remainingCommands.isEmpty()) {
+                    return new ArgumentsPrompt(context, sender, remainingCommands, opPlayer);
+                } else {
+                    return END_OF_CONVERSATION;
+                }
+            }
+            return this;
+        }
+
+        public String[] getArguments() {
+            return arguments;
+        }
+
+        private void runCommand(String command) {
+            boolean isOp = sender.isOp();
+            if (!isOp && opPlayer) {
+                sender.setOp(true);
+            }
+            context.getController().getPlugin().getServer().dispatchCommand(sender, command);
         }
     }
 }
