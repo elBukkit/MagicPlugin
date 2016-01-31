86b36aaf98cb0e992ee4c2e99cbb5c7e341d3d25
killme <killme@graphox.fake>
2016-01-31 01:53:27 +0100

    Add default serial version uid to WildcardHashSet.

--- src/main/java/com/elmakers/mine/bukkit/block/WildcardHashSet.java
+++ src/main/java/com/elmakers/mine/bukkit/block/WildcardHashSet.java
@@ -3,6 +3,8 @@ package com.elmakers.mine.bukkit.block;
 import java.util.HashSet;
 
 public class WildcardHashSet<T> extends HashSet<T> {
+    private static final long serialVersionUID = 1L;
+
     @Override
     public boolean contains(Object o) {
         return true;
