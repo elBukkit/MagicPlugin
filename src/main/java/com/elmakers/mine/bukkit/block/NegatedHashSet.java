2f75ea3ae052eeea94970a0ed1c853ff0ec04699
killme <killme@graphox.fake>
2016-01-31 01:24:41 +0100

    Fix warnings in NegatedHashSet.

--- src/main/java/com/elmakers/mine/bukkit/block/NegatedHashSet.java
+++ src/main/java/com/elmakers/mine/bukkit/block/NegatedHashSet.java
@@ -2,7 +2,9 @@ package com.elmakers.mine.bukkit.block;
 
 import java.util.HashSet;
 
-public class NegatedHashSet<T> extends HashSet {
+public class NegatedHashSet<T> extends HashSet<T> {
+    private static final long serialVersionUID = 1L;
+
     @Override
     public boolean contains(Object o) {
         return !super.contains(o);
