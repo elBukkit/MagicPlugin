1ec7834e6837222325f83bc4c883cca694f15231
Nathan Wolf <nathan@elmakers.com>
2016-02-05 11:22:22 -0800

    Add rand() function to equation parser

--- src/main/java/com/elmakers/mine/bukkit/math/EquationTransform.java
+++ src/main/java/com/elmakers/mine/bukkit/math/EquationTransform.java
@@ -3,12 +3,15 @@ package com.elmakers.mine.bukkit.math;
 import com.elmakers.mine.bukkit.api.math.Transform;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
+import net.objecthunter.exp4j.function.Function;
 import org.bukkit.configuration.ConfigurationSection;
 
+import java.util.Random;
 import java.util.logging.Level;
 
 public class EquationTransform implements Transform {
     private Expression expression;
+    private static Function randFunction;
 
     @Override
     public void load(ConfigurationSection parameters) {
@@ -25,7 +28,18 @@ public class EquationTransform implements Transform {
 
     public void setEquation(String equation) {
         try {
+            if (randFunction == null) {
+                randFunction = new Function("rand", 2) {
+                    private Random random = new Random();
+                    
+                    @Override
+                    public double apply(double... args) {
+                        return random.nextDouble() * (args[1] - args[0]) + args[0];
+                    }
+                };
+            }
             expression = new ExpressionBuilder(equation)
+                .function(randFunction)
                 .variables("t")
                 .build();
         } catch (Exception ex) {
