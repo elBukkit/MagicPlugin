6b0870b6b93d36a90035b71e355c4004008f1f85
Nathan Wolf <nathan@elmakers.com>
2016-02-17 13:44:17 -0800

    Move Transform interface to EffectLib

--- src/main/java/com/elmakers/mine/bukkit/api/math/Transform.java
+++
@@ -1,8 +0,0 @@
-package com.elmakers.mine.bukkit.api.math;
-
-import org.bukkit.configuration.ConfigurationSection;
-
-public interface Transform {
-    public void load(ConfigurationSection parameters);
-    public double get(double t);
-}
