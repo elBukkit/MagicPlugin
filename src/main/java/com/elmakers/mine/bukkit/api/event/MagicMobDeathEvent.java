d5f48482c347dad2d7ebc50951c1e9de7cb4a0d8
Nathan Wolf <nathan@elmakers.com>
2016-02-29 12:03:59 -0800

    Fixes and improvements to Magic Mob death event, add killer info

--- src/main/java/com/elmakers/mine/bukkit/api/event/MagicMobDeathEvent.java
+++ src/main/java/com/elmakers/mine/bukkit/api/event/MagicMobDeathEvent.java
@@ -1,23 +1,56 @@
 package com.elmakers.mine.bukkit.api.event;
 
 import com.elmakers.mine.bukkit.api.entity.EntityData;
+import com.elmakers.mine.bukkit.api.magic.Mage;
+import com.elmakers.mine.bukkit.api.magic.MageController;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 public class MagicMobDeathEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     
-    private EntityDeathEvent deathEvent;
-    private EntityData entityData;
+    private final EntityDeathEvent deathEvent;
+    private final EntityData entityData;
+    private final MageController controller;
+    private Mage mage;
+    private Player player;
     
-    public MagicMobDeathEvent(EntityData entityData, EntityDeathEvent deathEvent) {
-        this.entityData =entityData;
+    public MagicMobDeathEvent(MageController controller, EntityData entityData, EntityDeathEvent deathEvent) {
+        this.controller = controller;
+        this.entityData = entityData;
         this.deathEvent = deathEvent;
+        
+        LivingEntity killed = deathEvent.getEntity();
+        EntityDamageEvent damageEvent = killed.getLastDamageCause();
+        if (damageEvent instanceof EntityDamageByEntityEvent)
+        {
+            EntityDamageByEntityEvent damageByEvent = (EntityDamageByEntityEvent)damageEvent;
+            Entity damagingEntity = damageByEvent.getDamager();
+            if (damagingEntity instanceof Projectile) {
+                Projectile projectile = (Projectile)damagingEntity;
+                damagingEntity = (LivingEntity)projectile.getShooter();
+            }
+            if (damagingEntity != null && damagingEntity instanceof Player)
+            {
+                player = (Player)damagingEntity;
+                mage = controller.getRegisteredMage(player.getUniqueId().toString());
+            }
+        }
     }
 
     @Override
     public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
         return handlers;
     }
 
@@ -27,5 +60,17 @@ public class MagicMobDeathEvent extends Event {
 
     public EntityData getEntityData() {
         return entityData;
+    }
+    
+    public MageController getController() {
+        return controller;
+    }
+    
+    public Mage getMage() {
+        return mage;
+    }
+    
+    public Player getPlayer() {
+        return player;
     }
 }
