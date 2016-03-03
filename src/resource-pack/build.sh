47c44ff5e7202b81da1741d07448556cdfa74e84
Nathan Wolf <nathan@elmakers.com>
2016-03-03 09:30:01 -0800

    Tweak Blaster and Goggles in RP, don't rebuild legacy RP's anymore

--- src/resource-pack/build.sh
+++ src/resource-pack/build.sh
@@ -16,7 +16,7 @@ cp -R ../../../common/assets/* .
 cd ..
 cp -R ../../survival/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-RP.zip *
+zip -r -X ../Magic-RP-6.zip *
 cd ..
 
 echo "** BUILDING POTTER **"
@@ -72,49 +72,4 @@ cp -R ../../stars/* .
 cp -R ../../survival/* .
 find . -name ".DS_Store" -type f -delete
 zip -r -X ../Magic-ffa-RP.zip *
-cd ..
-
-# 4.0 Configs include outdated models/textures
-
-echo "** BUILDING 4.0 SURVIVAL **"
-
-mkdir survival-4
-cd survival-4
-mkdir assets
-cd assets
-cp -R ../../../common/assets/* .
-cp -R ../../../4.0/common/assets/* .
-cd ..
-cp -R ../../survival/* .
-cp -R ../../4.0/survival/* .
-find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-RP-4.zip *
-cd ..
-
-echo "** BUILDING 4.0 POTTER **"
-mkdir potter-4
-cd potter-4
-mkdir assets
-cd assets
-cp -R ../../../common/assets/* .
-cp -R ../../../4.0/common/assets/* .
-cd ..
-cp -R ../../potter/* .
-cp -R ../../4.0/potter/* .
-find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-potter-RP-4.zip *
-cd ..
-
-echo "** BUILDING 4.0 BENDING **"
-mkdir bending-4
-cd bending-4
-mkdir assets
-cd assets
-cp -R ../../../common/assets/* .
-cp -R ../../../4.0/common/assets/* .
-cd ..
-cp -R ../../bending/* .
-cp -R ../../4.0/bending/* .
-find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-bending-RP-4.zip *
 cd ..
