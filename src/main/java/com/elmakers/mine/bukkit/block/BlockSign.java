6194295bbc100c03ebd3fd37bb141ae5fc9a6887
Nathan Wolf <nathan@elmakers.com>
2016-03-05 12:11:58 -0800

    Catch JSON parse exceptions when reading sign data

--- src/main/java/com/elmakers/mine/bukkit/block/BlockSign.java
+++ src/main/java/com/elmakers/mine/bukkit/block/BlockSign.java
@@ -3,8 +3,10 @@ package com.elmakers.mine.bukkit.block;
 import com.elmakers.mine.bukkit.utility.InventoryUtils;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import org.bukkit.Bukkit;
 
 import java.io.StringReader;
+import java.util.logging.Level;
 
 public class BlockSign extends BlockExtraData {
     protected String[] lines = new String[4];
@@ -20,13 +22,18 @@ public class BlockSign extends BlockExtraData {
         for (int i = 0; i < 4; ++i) {
             String line = InventoryUtils.getMeta(tag, "Text" + (i + 1));
             if (line != null && !line.startsWith("{")) {
-                if (gson == null) {
-                    gson = new Gson();
+                try {
+                    if (gson == null) {
+                        gson = new Gson();
+                    }
+                    JsonReader reader = new JsonReader(new StringReader(line));
+                    reader.setLenient(true);
+                    line = gson.fromJson(reader, String.class);
+                    if (line == null) line = "";
+                } catch (Exception ex) {
+                    Bukkit.getLogger().log(Level.WARNING, "Error loading sign data", ex);
+                    line = "";
                 }
-                JsonReader reader = new JsonReader(new StringReader(line));
-                reader.setLenient(true);
-                line = gson.fromJson(reader, String.class);
-                if (line == null) line = "";
             }
             this.lines[i] = line;
         }
