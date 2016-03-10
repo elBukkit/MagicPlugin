6cde2050e11ae757ca24e667b7b378e0bba990a8
Jeremy Wood <farachan@gmail.com>
2016-03-10 10:59:02 -0500

    Added MageLoadEvent.

---
+++ src/main/java/com/elmakers/mine/bukkit/api/event/MageLoadEvent.java
@@ -0,0 +1,37 @@
+package com.elmakers.mine.bukkit.api.event;
+
+import com.elmakers.mine.bukkit.api.magic.Mage;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * A custom event that fires after a Mage's data is loaded.
+ */
+public class MageLoadEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Mage mage;
+    private final boolean firstTime;
+
+    public MageLoadEvent(Mage mage, boolean firstTime) {
+        this.mage = mage;
+        this.firstTime = firstTime;
+    }
+
+    public Mage getMage() {
+        return mage;
+    }
+
+    public boolean isFirstTime() {
+        return firstTime;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
