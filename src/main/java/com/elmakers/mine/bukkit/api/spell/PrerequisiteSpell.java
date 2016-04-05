fd89c056a8e684cd2839d2026dc16e627a6aa5b2
Jeremy Wood <farachan@gmail.com>
2016-04-05 13:42:10 -0400

    Moved prereq check logic into PrerequisiteSpell.

--- src/main/java/com/elmakers/mine/bukkit/api/spell/PrerequisiteSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/api/spell/PrerequisiteSpell.java
@@ -1,5 +1,11 @@
 package com.elmakers.mine.bukkit.api.spell;
 
+import com.elmakers.mine.bukkit.api.wand.Wand;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 /**
  * Represents a spell that is required to learn another spell or path.
  */
@@ -19,5 +25,52 @@ public class PrerequisiteSpell {
 
     public long getProgressLevel() {
         return progressLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "PrerequisiteSpell{" +
+                "spellKey=" + spellKey.getKey() +
+                ", progressLevel=" + progressLevel +
+                '}';
+    }
+
+    public static Collection<PrerequisiteSpell> getMissingRequirements(Wand wand, SpellTemplate spell) {
+        Collection<PrerequisiteSpell> missingRequirements = new ArrayList<PrerequisiteSpell>(spell.getPrerequisiteSpells());
+        if (wand == null) {
+            return missingRequirements;
+        }
+        Iterator<PrerequisiteSpell> it = missingRequirements.iterator();
+        while (it.hasNext()) {
+            PrerequisiteSpell prereq = it.next();
+            Spell mageSpell = wand.getSpell(prereq.getSpellKey().getKey());
+            if (isSpellAPrereq(mageSpell, prereq)) {
+                it.remove();
+            }
+        }
+        return missingRequirements;
+    }
+
+    public static boolean hasPrerequisites(Wand wand, SpellTemplate spell) {
+        if (spell == null) {
+            return true;
+        }
+        if (wand == null) {
+            return false;
+        }
+        for (PrerequisiteSpell prereq : spell.getPrerequisiteSpells()) {
+            Spell mageSpell = wand.getSpell(prereq.getSpellKey().getKey());
+            if (!isSpellAPrereq(mageSpell, prereq)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isSpellAPrereq(Spell spell, PrerequisiteSpell prerequisiteSpell) {
+        return spell != null
+                && (spell.getSpellKey().getLevel() > prerequisiteSpell.getSpellKey().getLevel()
+                || (spell.getProgressLevel() >= prerequisiteSpell.getProgressLevel()
+                && spell.getSpellKey().getLevel() == prerequisiteSpell.getSpellKey().getLevel()));
     }
 }
