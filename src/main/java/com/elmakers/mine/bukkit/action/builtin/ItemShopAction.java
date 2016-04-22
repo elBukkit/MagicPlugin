52ac4e582e1be27972f1f35c82e56def4cd8d168
Nathan Wolf <nathan@elmakers.com>
2016-04-22 10:40:54 -0700

    ItemShop will look up item value if not set. Now supports a simple list of items.

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/ItemShopAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/ItemShopAction.java
@@ -6,8 +6,9 @@ import com.elmakers.mine.bukkit.api.magic.MageController;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
-import net.md_5.bungee.api.ChatColor;
+import de.slikey.effectlib.util.ConfigUtils;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -15,6 +16,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class ItemShopAction extends BaseShopAction
 {
@@ -31,47 +33,54 @@ public class ItemShopAction extends BaseShopAction
                 ConfigurationSection itemSection = ConfigurationUtils.getConfigurationSection(parameters, "items");
                 Collection<String> itemKeys = itemSection.getKeys(false);
                 for (String itemKey : itemKeys) {
-                    items.add(parseItemKey(spell.getController(), itemKey, itemSection.getDouble(itemKey)));
+                    items.add(parseItemKey(spell.getController(), itemKey, itemSection.getDouble(itemKey, -1)));
                 }
             } else {
-                Collection<ConfigurationSection> itemList = ConfigurationUtils.getNodeList(parameters, "items");
-                for (ConfigurationSection itemConfig : itemList)
+                List<?> itemList = parameters.getList("items");
+                for (Object itemEntry : itemList)
                 {
-                    ShopItem shopItem = null;
-                    if (itemConfig != null) {
-                        double cost = itemConfig.getDouble("cost");
-                        if (itemConfig.isString("item")) {
-                            shopItem = parseItemKey(spell.getController(), itemConfig.getString("item"), cost);
-                            if (shopItem != null) {
-                                String name = itemConfig.getString("name");
-                                List<String> lore = ConfigurationUtils.getStringList(itemConfig, "lore");
+                    if (itemEntry instanceof String) {
+                        String itemKey = (String)itemEntry;
+                        items.add(parseItemKey(spell.getController(), itemKey, -1));
+                    } else if (itemEntry instanceof ConfigurationSection || itemEntry instanceof Map) {
+                        ConfigurationSection itemConfig = (itemEntry instanceof ConfigurationSection) ?
+                                (ConfigurationSection)itemEntry : ConfigUtils.toConfigurationSection((Map)itemEntry); 
+                        ShopItem shopItem = null;
+                        if (itemConfig != null) {
+                            double cost = itemConfig.getDouble("cost");
+                            if (itemConfig.isString("item")) {
+                                shopItem = parseItemKey(spell.getController(), itemConfig.getString("item"), cost);
+                                if (shopItem != null) {
+                                    String name = itemConfig.getString("name");
+                                    List<String> lore = ConfigurationUtils.getStringList(itemConfig, "lore");
 
-                                if (name != null || lore != null) {
-                                    ItemStack item = shopItem.getItem();
-                                    ItemMeta meta = item.getItemMeta();
-                                    if (name != null) {
-                                        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
-                                    }
-                                    if (lore != null) {
-                                        List<String> translatedLore = new ArrayList<String>();
-                                        for (String line : lore) {
-                                            if (line != null) {
-                                                translatedLore.add(ChatColor.translateAlternateColorCodes('&', line));
-                                            }
+                                    if (name != null || lore != null) {
+                                        ItemStack item = shopItem.getItem();
+                                        ItemMeta meta = item.getItemMeta();
+                                        if (name != null) {
+                                            meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
                                         }
-                                        meta.setLore(translatedLore);
+                                        if (lore != null) {
+                                            List<String> translatedLore = new ArrayList<String>();
+                                            for (String line : lore) {
+                                                if (line != null) {
+                                                    translatedLore.add(ChatColor.translateAlternateColorCodes('&', line));
+                                                }
+                                            }
+                                            meta.setLore(translatedLore);
+                                        }
+                                        item.setItemMeta(meta);
                                     }
-                                    item.setItemMeta(meta);
+                                }
+                            } else {
+                                ItemStack itemStack = itemConfig.getItemStack("item");
+                                if (itemStack != null) {
+                                    shopItem = new ShopItem(itemStack, cost);
                                 }
                             }
-                        } else {
-                            ItemStack itemStack = itemConfig.getItemStack("item");
-                            if (itemStack != null) {
-                                shopItem = new ShopItem(itemStack, cost);
-                            }
                         }
+                        items.add(shopItem);
                     }
-                    items.add(shopItem);
                 }
             }
         }
@@ -95,8 +104,15 @@ public class ItemShopAction extends BaseShopAction
         }
         ItemStack item = controller.createItem(itemKey, castsSpells);
         if (item == null) return null;
-        
         item.setAmount(amount);
+        if (worth < 0) {
+            Double defaultWorth = controller.getWorth(item);
+            
+            org.bukkit.Bukkit.getLogger().info("Checking worth: " + defaultWorth + " of " + item);
+            
+            worth = defaultWorth == null ? 0 : defaultWorth;
+        }
+        
         return new ShopItem(item, worth);
     }
     
