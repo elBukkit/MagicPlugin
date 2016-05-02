56dcd7218a1f2b2a759f33d15627f52bab67d429
Nathan Wolf <nathan@elmakers.com>
2016-05-01 19:50:47 -0700

    Interpret an empty string in upgrade_required_path as "no requirement"

--- src/main/java/com/elmakers/mine/bukkit/spell/BaseSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/spell/BaseSpell.java
@@ -747,6 +747,9 @@ public abstract class BaseSpell implements MageSpell, Cloneable {
         // Upgrade path information
         // The actual upgrade spell will be set externally.
         requiredUpgradePath = node.getString("upgrade_required_path");
+        if (requiredUpgradePath != null && requiredUpgradePath.isEmpty()) {
+            requiredUpgradePath = null;
+        }
         requiredUpgradeCasts = node.getLong("upgrade_required_casts");
         List<String> pathTags = ConfigurationUtils.getStringList(node, "upgrade_required_path_tags");
         if (pathTags == null || pathTags.isEmpty()) {
