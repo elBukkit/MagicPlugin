cfb0adda555d6b016ee7867916dd98cc7b7bf79d
SexyToad <SexyToad@AvatarMC.com>
2016-06-09 14:56:20 -0700

    Fixed random formatting issue

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/ChangeContextAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/ChangeContextAction.java
@@ -218,7 +218,7 @@ public class ChangeContextAction extends CompoundAction {
         if (sourceDirectionOffset != null)
         {
             sourceLocation.setDirection(direction.add(sourceDirectionOffset));
-        }      
+        }
         if (sourceDirectionSpeed != null)
         {
             sourceLocation = sourceLocation.add(direction.clone().multiply(sourceDirectionSpeed));
