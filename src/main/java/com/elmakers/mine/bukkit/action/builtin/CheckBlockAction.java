1818c7a4f4a369bc69f5c706973e2e4e3fec1d19
S-Toad (Alex) <AlexAyala9975@hotmail.com>
2016-07-03 00:39:55 -0700

    Revert "Formatting"
    This reverts commit 08b27a9b8fee931016f9772d8b538d18cf414caf.
    
    Want to keep in original code style

--- src/main/java/com/elmakers/mine/bukkit/action/builtin/CheckBlockAction.java
+++ src/main/java/com/elmakers/mine/bukkit/action/builtin/CheckBlockAction.java
@@ -13,62 +13,61 @@ import org.bukkit.configuration.ConfigurationSection;
 import java.util.Set;
 
 public class CheckBlockAction extends CompoundAction {
-	private Set<Material> allowed;
-	private boolean returnCosts;
-
-	private SpellResult failSpellResult;
-
-	@Override
-	public void initialize(Spell spell, ConfigurationSection parameters) {
-		super.initialize(spell, parameters);
-		allowed = spell.getController().getMaterialSet(parameters.getString("allowed"));
-		returnCosts = parameters.getBoolean("return_costs", false);
-
+    private Set<Material> allowed;
+    private boolean returnCosts;
+    
+    private SpellResult failSpellResult;
+    @Override
+    public void initialize(Spell spell, ConfigurationSection parameters)
+    {
+        super.initialize(spell, parameters);
+        allowed = spell.getController().getMaterialSet(parameters.getString("allowed"));
+        returnCosts = parameters.getBoolean("return_costs", false);
+		
 		failSpellResult = returnCosts ? SpellResult.CANCELLED : SpellResult.NO_TARGET;
-	}
+    }
+    
+    protected boolean isAllowed(CastContext context) {
+        MaterialBrush brush = context.getBrush();
+        Block block = context.getTargetBlock();
+        if (block == null) {
+            return false;
+        }
+        if (allowed != null) {
+            if (!allowed.contains(block.getType())) return false;
+        } else {
+            if (brush != null && brush.isErase()) {
+                if (!context.hasBreakPermission(block)) {
+                    return false;
+                }
+            } else {
+                if (!context.hasBuildPermission(block)) {
+                    return false;
+                }
+            }
+            if (!context.isDestructible(block)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public SpellResult step(CastContext context) {
+        boolean allowed = isAllowed(context);
+        ActionHandler actions = getHandler("actions");
+        if (actions == null || actions.size() == 0) {
+            return allowed ? SpellResult.CAST : SpellResult.STOP;
+        }
+        
+        if (!allowed) {
+            return failSpellResult;
+        }
+        return startActions();
+    }
 
-	protected boolean isAllowed(CastContext context) {
-		MaterialBrush brush = context.getBrush();
-		Block block = context.getTargetBlock();
-		if (block == null) {
-			return false;
-		}
-		if (allowed != null) {
-			if (!allowed.contains(block.getType()))
-				return false;
-		} else {
-			if (brush != null && brush.isErase()) {
-				if (!context.hasBreakPermission(block)) {
-					return false;
-				}
-			} else {
-				if (!context.hasBuildPermission(block)) {
-					return false;
-				}
-			}
-			if (!context.isDestructible(block)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public SpellResult step(CastContext context) {
-		boolean allowed = isAllowed(context);
-		ActionHandler actions = getHandler("actions");
-		if (actions == null || actions.size() == 0) {
-			return allowed ? SpellResult.CAST : SpellResult.STOP;
-		}
-
-		if (!allowed) {
-			return failSpellResult;
-		}
-		return startActions();
-	}
-
-	@Override
-	public boolean requiresTarget() {
-		return false;
-	}
+    @Override
+    public boolean requiresTarget() {
+        return false;
+    }
 }
