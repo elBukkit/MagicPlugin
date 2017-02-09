7d84e32eced9534e50d38a4190989d771c1a3810
Nathan Wolf <nathan@elmakers.com>
2017-02-08 16:42:26 -0800

    Add DropItem action

---
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/DropItemAction.java
@@ -0,0 +1,57 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.BaseSpellAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.magic.MageController;
+import com.elmakers.mine.bukkit.api.magic.MagicAPI;
+import com.elmakers.mine.bukkit.api.spell.Spell;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.magic.MagicPlugin;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+
+public class DropItemAction extends BaseSpellAction
+{
+    private ItemStack item = null;
+
+    @Override
+    public void prepare(CastContext context, ConfigurationSection parameters) {
+        super.prepare(context, parameters);
+        MageController controller = context.getController();
+
+        String itemKey = parameters.getString("item");
+        item = controller.createItem(itemKey);
+        if (item == null) {
+            context.getLogger().warning("Invalid item: " + itemKey);
+        }
+    }
+
+    @Override
+    public SpellResult perform(CastContext context) {
+        if (item == null) {
+            return SpellResult.FAIL;
+        }
+        context.getTargetLocation().getWorld().dropItem(context.getTargetLocation(), item);
+        return SpellResult.CAST;
+	}
+
+    @Override
+    public void getParameterNames(Spell spell, Collection<String> parameters)
+    {
+        super.getParameterNames(spell, parameters);
+        parameters.add("item");
+    }
+
+    @Override
+    public void getParameterOptions(Spell spell, String parameterKey, Collection<String> examples)
+    {
+        if (parameterKey.equals("item")) {
+            MagicAPI api = MagicPlugin.getAPI();
+            examples.addAll(api.getController().getItemKeys());
+        } else {
+            super.getParameterOptions(spell, parameterKey, examples);
+        }
+    }
+}
