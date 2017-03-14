87a52a8242eea449204426fbc56d3ae4e0849fa0
Nathan Wolf <nathan@elmakers.com>
2017-03-14 11:00:54 -0700

    Add an enum to represent different storage options for magic properties

---
+++ MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/magic/MagicPropertyType.java
@@ -0,0 +1,20 @@
+package com.elmakers.mine.bukkit.api.magic;
+
+/**
+ * Specifies different types of property holders.
+ *
+ * Properties in Magic can be configured to save in one of these locations.
+ *
+ * Generally this is an inheritance chain, going like
+ *
+ * Mage->Class->(SubClass...)->SubClass->Wand
+ *
+ * But for most purposes properties are all merged together along this chain,
+ * and generally speaking a property may not appear in more than one place.
+ *
+ * The exception to this rule is subclasses, since there maybe many of those, the
+ * ones further down the chain will take precedent.
+ */
+public enum MagicPropertyType {
+    WAND, SUBCLASS, CLASS, MAGE
+}
