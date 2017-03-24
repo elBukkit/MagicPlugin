c65e017266a3247c1e5ad16df88ea76dac81933b
Nathan Wolf <nathan@elmakers.com>
2017-03-24 07:44:53 -0700

    Change active mage class save key to be consistent

--- Magic/src/main/java/com/elmakers/mine/bukkit/data/ConfigurationMageDataStore.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/data/ConfigurationMageDataStore.java
@@ -164,7 +164,7 @@ public abstract class ConfigurationMageDataStore implements MageDataStore {
                 ConfigurationUtils.addConfigurations(classSection, entry.getValue());
             }
         }
-        saveFile.set("activeClass", mage.getActiveClass());
+        saveFile.set("active_class", mage.getActiveClass());
     }
 
     @Override
@@ -227,7 +227,7 @@ public abstract class ConfigurationMageDataStore implements MageDataStore {
             }
         }
         data.setClassProperties(classProperties);
-        data.setActiveClass(saveFile.getString("activeClass"));
+        data.setActiveClass(saveFile.getString("active_class"));
 
         // Load extra data
         data.setExtraData(saveFile.getConfigurationSection("data"));
