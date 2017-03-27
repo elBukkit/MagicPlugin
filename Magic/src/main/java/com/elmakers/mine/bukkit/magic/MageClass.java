685fce533d3be7c9ec03e92ba7942add7be52c19
Nathan Wolf <nathan@elmakers.com>
2017-03-27 13:46:44 -0700

    Decided to collapse MageClass and MageClassProperties, the distinction doesn't seem like it was going to be useful.

--- Magic/src/main/java/com/elmakers/mine/bukkit/magic/MageClass.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/magic/MageClass.java
@@ -1,13 +1,48 @@
 package com.elmakers.mine.bukkit.magic;
 
-import javax.annotation.Nonnull;
+import com.elmakers.mine.bukkit.utility.ConfigurationUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 
-public class MageClass extends MageClassProperties implements com.elmakers.mine.bukkit.api.magic.MageClass {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class MageClass extends BaseMagicConfigurable implements com.elmakers.mine.bukkit.api.magic.MageClass  {
+    protected final MageClassTemplate template;
+    protected final MageProperties mage;
+    private MageClass parent;
+
     public MageClass(@Nonnull Mage mage, @Nonnull MageClassTemplate template) {
-        super(mage.getProperties(), template, mage.getController());
+        super(template.hasParent() ? MagicPropertyType.SUBCLASS : MagicPropertyType.CLASS, mage.getController());
+        this.template = template;
+        this.mage = mage.getProperties();
     }
 
-    public MageClassTemplate getTemplate() {
+    @Override
+    protected void rebuildEffectiveConfiguration(@Nonnull ConfigurationSection effectiveConfiguration) {
+        ConfigurationSection templateConfiguration = template.getEffectiveConfiguration();
+        ConfigurationUtils.addConfigurations(effectiveConfiguration, templateConfiguration, false);
+        if (parent != null) {
+            ConfigurationSection parentConfiguration = parent.getEffectiveConfiguration();
+            ConfigurationUtils.addConfigurations(effectiveConfiguration, parentConfiguration, false);
+        } else {
+            // If we have a parent, it has already incorporated Mage data
+            ConfigurationSection mageConfiguration = mage.getEffectiveConfiguration();
+            ConfigurationUtils.addConfigurations(effectiveConfiguration, mageConfiguration, false);
+        }
+    }
+
+    public @Nonnull MageClassTemplate getTemplate() {
         return template;
+    }
+
+    public @Nullable MageClass getParent() {
+        return parent;
+    }
+
+    public void setParent(@Nonnull MageClass parent) {
+        this.parent = parent;
+        this.dirty = true;
     }
 }
