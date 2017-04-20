f1da3bbf9cff6ef587a634e687949b7d3d910ee1
Nathan Wolf <nathan@elmakers.com>
2017-04-20 07:38:59 -0700

    Handle the case of an offhand cast removing the offhand wand

--- Magic/src/main/java/com/elmakers/mine/bukkit/magic/Mage.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/magic/Mage.java
@@ -1141,7 +1141,7 @@ public class Mage implements CostReducer, com.elmakers.mine.bukkit.api.magic.Mag
                     castResult = offhandWand.cast();
 
                     // Don't swing arm is cast is from right-click
-                    if (offhandWand.getRightClickAction() != WandAction.CAST) {
+                    if (offhandWand != null && offhandWand.getRightClickAction() != WandAction.CAST) {
                         CompatibilityUtils.swingOffhand(player, OFFHAND_CAST_RANGE);
                     }
                 } catch (Exception ex) {
