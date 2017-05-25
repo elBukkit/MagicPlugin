5757ce5b8dd9e469c206c7047e6d4d94dd922c8b
Nathan Wolf <nathan@elmakers.com>
2017-05-24 17:32:20 -0700

    Make sure to deactivate player wands when joining a mob arena

--- Magic/src/main/java/com/elmakers/mine/bukkit/integration/MobArenaManager.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/integration/MobArenaManager.java
@@ -1,13 +1,19 @@
 package com.elmakers.mine.bukkit.integration;
 
+import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.magic.MageController;
+import com.garbagemule.MobArena.events.ArenaPlayerJoinEvent;
 import com.garbagemule.MobArena.util.ItemParser;
 import com.garbagemule.MobArena.util.ItemProvider;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Set;
 
-public class MobArenaManager implements ItemProvider {
+public class MobArenaManager implements ItemProvider, Listener {
     private final MageController controller;
 
     public MobArenaManager(MageController controller) {
@@ -20,6 +26,8 @@ public class MobArenaManager implements ItemProvider {
             String mobKey = mob.toLowerCase().replaceAll("[-_\\.]", "");
             new MagicMACreature(controller, mobKey, controller.getMob(mob));
         }
+
+        Bukkit.getPluginManager().registerEvents(this, controller.getPlugin());
     }
 
     @Override
@@ -27,5 +35,14 @@ public class MobArenaManager implements ItemProvider {
         if (!s.startsWith("magic:")) return null;
         s = s.substring(6);
         return controller.createItem(s);
+    }
+
+    @EventHandler
+    public void onPlayerJoinArena(ArenaPlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Mage mage = controller.getRegisteredMage(player.getUniqueId().toString());
+        if (mage != null) {
+            mage.deactivate();
+        }
     }
 }
