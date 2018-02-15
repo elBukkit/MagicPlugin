9f8f84a8b66ce38d768644e3a8fe82af6c483cf4
Nathan Wolf <nathan@elmakers.com>
2018-02-15 12:14:57 -0800

    MageClassTemplate interface is a property container

--- MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/magic/MageClassTemplate.java
+++ MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/magic/MageClassTemplate.java
@@ -1,6 +1,6 @@
 package com.elmakers.mine.bukkit.api.magic;
 
-public interface MageClassTemplate {
+public interface MageClassTemplate extends MagicProperties {
     String getName();
     String getDescription();
 }
