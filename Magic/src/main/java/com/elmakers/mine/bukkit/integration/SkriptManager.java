c9e2365fd367002527a3cb8425a3390ef9174dc3
Nathan Wolf <nathan@elmakers.com>
2018-03-05 20:59:17 -0800

    Add "has wand" and "is class" conditionals

--- Magic/src/main/java/com/elmakers/mine/bukkit/integration/SkriptManager.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/integration/SkriptManager.java
@@ -7,6 +7,8 @@ import ch.njol.skript.util.Getter;
 import com.elmakers.mine.bukkit.api.event.CastEvent;
 import com.elmakers.mine.bukkit.api.event.PreCastEvent;
 import com.elmakers.mine.bukkit.api.magic.MageController;
+import com.elmakers.mine.bukkit.integration.skript.CondHasWand;
+import com.elmakers.mine.bukkit.integration.skript.CondIsClass;
 import com.elmakers.mine.bukkit.integration.skript.ExprCaster;
 import com.elmakers.mine.bukkit.integration.skript.ExprTargets;
 import com.elmakers.mine.bukkit.integration.skript.SkriptCastEffect;
@@ -32,6 +34,18 @@ public class SkriptManager {
 				"cast [the] [spell] %string% [by %-commandsenders%] [with %-string%]",
 				"(let|make) %commandsenders% cast [[the] spell] %string% [with %-string%]");
 
+		Skript.registerCondition(CondIsClass.class, "%entities% (is|are) class %strings%", "%entities% (isn't|is not|aren't|are not) class %strings%");
+		Skript.registerCondition(CondHasWand.class,
+	"[%entities%] ha(s|ve) wand [%-strings%] in [main] hand",
+			"[%entities%] (is|are) holding wand [%-strings%] [in main hand]",
+			"[%entities%] ha(s|ve) wand [%-strings%] in off[(-| )]hand",
+			"[%entities%] (is|are) holding wand [%-strings%] in off[(-| )]hand",
+			"[%entities%] (ha(s|ve) not|do[es]n't have) wand [%-strings%] in [main] hand",
+			"[%entities%] (is not|isn't) holding wand [%-strings%] [in main hand]",
+			"[%entities%] (ha(s|ve) not|do[es]n't have) wand [%-strings%] in off[(-| )]hand",
+			"[%entities%] (is not|isn't) holding wand [%-strings%] in off[(-| )]hand"
+		);
+
 		EventValues.registerEventValue(CastEvent.class, Player.class, new Getter<Player, CastEvent>() {
 			@Override
 			public Player get(final CastEvent e) {
@@ -49,6 +63,10 @@ public class SkriptManager {
 		Skript.registerExpression(ExprCaster.class, Entity.class, ExpressionType.SIMPLE, "[the] (caster)");
 		Skript.registerExpression(ExprTargets.class, Entity.class, ExpressionType.SIMPLE, "[the] (targets)");
 
-        plugin.getLogger().info("Skript found, added cast event and effect");
+        plugin.getLogger().info("Skript found:");
+        plugin.getLogger().info("  Added events: cast, casting");
+        plugin.getLogger().info("  Added expressions: caster, targets");
+        plugin.getLogger().info("  Added conditionals: has wand, is class");
+        plugin.getLogger().info("  Added effect: cast");
     }
 }
