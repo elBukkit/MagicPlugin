b8c6280cc75615d72849e3d2e4fc210c2ad69ccb
Tyler Grissom <minestein123@gmail.com>
2018-04-11 18:47:55 -0500

    Added ClearChatAction

---
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/ClearChatAction.java
@@ -0,0 +1,35 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.BaseSpellAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import org.bukkit.entity.Player;
+
+/**
+ * Copyright Tyler Grissom 2018
+ */
+public class ClearChatAction extends BaseSpellAction {
+
+    @Override
+    public SpellResult perform(CastContext context) {
+        if (!(context.getTargetEntity() instanceof Player)) return SpellResult.PLAYER_REQUIRED;
+
+        Player p = ((Player) context.getTargetEntity());
+
+        for (int i = 0; i < 100; i++) {
+            p.sendMessage(" ");
+        }
+
+        return SpellResult.CAST;
+    }
+
+    @Override
+    public boolean requiresTarget() {
+        return true;
+    }
+
+    @Override
+    public boolean requiresTargetEntity() {
+        return true;
+    }
+}
