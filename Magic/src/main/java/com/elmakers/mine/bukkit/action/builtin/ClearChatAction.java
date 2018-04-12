aa8b96f33de791ca52d4a620ac9b6b7cc32c81d3
Tyler Grissom <minestein123@gmail.com>
2018-04-11 19:17:35 -0500

    Removed header and passed checkstyle

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/ClearChatAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/ClearChatAction.java
@@ -1,20 +1,18 @@
 package com.elmakers.mine.bukkit.action.builtin;
 
+import org.bukkit.entity.Player;
+
 import com.elmakers.mine.bukkit.action.BaseSpellAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
-import org.bukkit.entity.Player;
 
-/**
- * Copyright Tyler Grissom 2018
- */
 public class ClearChatAction extends BaseSpellAction {
 
     @Override
     public SpellResult perform(CastContext context) {
         if (!(context.getTargetEntity() instanceof Player)) return SpellResult.PLAYER_REQUIRED;
 
-        Player p = ((Player) context.getTargetEntity());
+        Player p = (Player) context.getTargetEntity();
 
         for (int i = 0; i < 100; i++) {
             p.sendMessage(" ");
