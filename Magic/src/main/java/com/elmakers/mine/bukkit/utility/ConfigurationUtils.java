a3324de0b7606fa87e689c349a45754a0862e8e6
Nathan Wolf <nathan@elmakers.com>
2018-04-12 07:42:33 -0700

    Prevent errors when passing empty parameters to a spell cast

--- Magic/src/main/java/com/elmakers/mine/bukkit/utility/ConfigurationUtils.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/utility/ConfigurationUtils.java
@@ -802,6 +802,7 @@ public class ConfigurationUtils extends ConfigUtils {
         {
             for (int i = 0; i < extraParameters.length - 1; i += 2)
             {
+                if (extraParameters[i] == null || extraParameters[i].isEmpty()) continue;
                 parameters.set(extraParameters[i], extraParameters[i + 1]);
             }
         }
