b5a714098d4935aba268ed7e7a98132957cfa45a
Nathan Wolf <nathan@elmakers.com>
2018-04-16 15:32:29 -0700

    Add magic_damage parameter to Kill action

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/KillAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/KillAction.java
@@ -1,20 +1,34 @@
 package com.elmakers.mine.bukkit.action.builtin;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 
 import com.elmakers.mine.bukkit.action.BaseSpellAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
+import com.elmakers.mine.bukkit.spell.BaseSpell;
+import com.elmakers.mine.bukkit.utility.CompatibilityUtils;
 
 public class KillAction extends BaseSpellAction
 {
+    private boolean magicDamage;
+
+    @Override
+    public void prepare(CastContext context, ConfigurationSection parameters) {
+        super.prepare(context, parameters);
+        magicDamage = parameters.getBoolean("magic_damage", false);
+    }
+
     @Override
     public SpellResult perform(CastContext context)
     {
         Entity entity = context.getTargetEntity();
-        if (!(entity instanceof Damageable))
-        {
+        if (!(entity instanceof Damageable)) {
             return SpellResult.NO_TARGET;
         }
 
@@ -24,7 +38,11 @@ public class KillAction extends BaseSpellAction
         }
         // Overkill to bypass protection
         context.registerModified(targetEntity);
-        targetEntity.damage(targetEntity.getMaxHealth() * 100);
+        if (magicDamage) {
+            CompatibilityUtils.magicDamage(targetEntity, targetEntity.getMaxHealth() * 100, context.getEntity());
+        } else {
+            targetEntity.damage(targetEntity.getMaxHealth() * 100);
+        }
         return SpellResult.CAST;
     }
 
@@ -38,5 +56,20 @@ public class KillAction extends BaseSpellAction
     public boolean requiresTargetEntity()
     {
         return true;
+    }
+
+    @Override
+    public void getParameterNames(Spell spell, Collection<String> parameters) {
+        super.getParameterNames(spell, parameters);
+        parameters.add("magic_damage");
+    }
+
+    @Override
+    public void getParameterOptions(Spell spell, String parameterKey, Collection<String> examples) {
+        if (parameterKey.equals("magic_damage")) {
+            examples.addAll(Arrays.asList((BaseSpell.EXAMPLE_BOOLEANS)));
+        } else {
+            super.getParameterOptions(spell, parameterKey, examples);
+        }
     }
 }
