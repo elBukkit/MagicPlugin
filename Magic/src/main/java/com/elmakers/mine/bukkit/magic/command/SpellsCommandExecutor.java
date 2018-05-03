e66c121ee13da7192612d54abc12b696b3c4dc51
Nathan Wolf <nathan@elmakers.com>
2018-05-03 06:49:58 -0700

    Don't show spell upgrades in /spells list

--- Magic/src/main/java/com/elmakers/mine/bukkit/magic/command/SpellsCommandExecutor.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/magic/command/SpellsCommandExecutor.java
@@ -46,6 +46,7 @@ public class SpellsCommandExecutor extends MagicTabExecutor {
         Player player = sender instanceof Player ? (Player)sender : null;
         for (SpellTemplate spell : spellVariants)
         {
+            if (spell.isHidden() || spell.getSpellKey().getLevel() > 1) continue;
             SpellCategory spellCategory = spell.getCategory();
             if (spellCategory != null && spellCategory.getKey().equalsIgnoreCase(category)
                 && (player == null || spell.hasCastPermission(player)))
@@ -126,7 +127,7 @@ public class SpellsCommandExecutor extends MagicTabExecutor {
             {
                 continue;
             }
-            if (spell.getCategory() == null) continue;
+            if (spell.getCategory() == null || spell.isHidden() || spell.getSpellKey().getLevel() > 1) continue;
             spellCount++;
         }
 
