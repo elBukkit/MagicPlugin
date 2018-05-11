218e3fd95f541b7c0d340949569ca888ea9170d9
killme <killme-git@ibts.me>
2018-05-11 09:06:14 +0200

    Clean up cast action

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/CastAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/CastAction.java
@@ -1,6 +1,9 @@
 package com.elmakers.mine.bukkit.action.builtin;
 
+import javax.annotation.Nullable;
+
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
 
 import com.elmakers.mine.bukkit.action.BaseSpellAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
@@ -9,31 +12,38 @@ import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 
 public class CastAction extends BaseSpellAction {
-    private String spellKey;
+    private @Nullable String spellKey;
     private boolean costFree;
     private boolean cooldownFree;
-    private ConfigurationSection spellParameters;
+    private @Nullable ConfigurationSection spellParameters;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
 
-        this.spellKey = parameters.getString("spell");
-        this.spellParameters = parameters.getConfigurationSection("spell_parameters");
-        this.costFree = parameters.getBoolean("cost_free", true);
-        this.cooldownFree = parameters.getBoolean("cooldown_free", false);
+        spellKey = parameters.getString("spell");
+        spellParameters = parameters.getConfigurationSection("spell_parameters");
+        costFree = parameters.getBoolean("cost_free", true);
+        cooldownFree = parameters.getBoolean("cooldown_free", false);
     }
 
     @Override
     public SpellResult perform(CastContext context) {
-        if (context.getTargetEntity() == null) {
-            return SpellResult.NO_TARGET;
-        }
         if (spellKey == null) {
             return SpellResult.FAIL;
         }
 
-        Mage targetMage = context.getController().getMage(context.getTargetEntity());
+        Entity target = context.getTargetEntity();
+        if (target == null) {
+            return SpellResult.NO_TARGET;
+        }
+
+        Mage targetMage = context.getController().getMage(target);
+
+        Spell spell = targetMage.getSpell(spellKey);
+        if (spell == null) {
+            return SpellResult.FAIL;
+        }
 
         boolean originalCostFree = targetMage.isCostFree();
         boolean originalCooldownFree = targetMage.isCooldownFree();
@@ -41,21 +51,14 @@ public class CastAction extends BaseSpellAction {
         targetMage.setCostFree(costFree);
         targetMage.setCooldownFree(cooldownFree);
 
-        boolean success = false;
         try {
-            Spell spell = targetMage.getSpell(spellKey);
-            if (spell != null) {
-                success = spell.cast(spellParameters);
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            success = false;
+            return spell.cast(spellParameters)
+                    ? SpellResult.CAST
+                    : SpellResult.FAIL;
+        } finally {
+            targetMage.setCostFree(originalCostFree);
+            targetMage.setCooldownFree(originalCooldownFree);
         }
-
-        targetMage.setCostFree(originalCostFree);
-        targetMage.setCooldownFree(originalCooldownFree);
-
-        return success ? SpellResult.CAST : SpellResult.FAIL;
     }
 
     @Override
