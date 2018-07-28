736c3a103f1dfb6e14f89cf5ddc51e88bd3f2620
killme <killme-git@ibts.me>
2018-07-28 13:15:52 +0200

    Add option to maintain velocity when teleporting

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/BaseTeleportAction.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import com.elmakers.mine.bukkit.api.action.CastContext;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
@@ -14,6 +15,7 @@ public abstract class BaseTeleportAction extends BaseSpellAction
     protected boolean safe = true;
     private boolean requiresBuildPermission = false;
     private boolean requiresExitPermission = true;
+    private boolean keepVelocity = false;
 
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
@@ -22,6 +24,7 @@ public abstract class BaseTeleportAction extends BaseSpellAction
         safe = parameters.getBoolean("safe", true);
         requiresBuildPermission = parameters.getBoolean("require_build", false);
         requiresExitPermission = parameters.getBoolean("require_exit", true);
+        keepVelocity = parameters.getBoolean("keep_velocity", keepVelocity);
     }
 
     protected SpellResult teleport(CastContext context, Entity entity, Location targetLocation) {
@@ -34,6 +37,28 @@ public abstract class BaseTeleportAction extends BaseSpellAction
         if (!context.canCast(targetLocation)) {
             return SpellResult.INSUFFICIENT_PERMISSION;
         }
-        return context.teleport(entity, targetLocation, verticalSearchDistance, safe) ? SpellResult.CAST : SpellResult.NO_TARGET;
+
+        Location sourceLocation = keepVelocity ? entity.getLocation() : null;
+        Vector sourceVelocity = keepVelocity ? entity.getVelocity() : null;
+        boolean result = context.teleport(
+                entity, targetLocation, verticalSearchDistance, safe);
+        if (result && keepVelocity) {
+            // Calculate how much we have rotated
+            double deltaYaw = targetLocation.getYaw() - sourceLocation.getYaw();
+            double angle = Math.toRadians(deltaYaw);
+
+            // Rotate the original velocity
+            double cos = Math.cos(angle);
+            double sin = Math.sin(angle);
+            double initialX = sourceVelocity.getX();
+            double initialZ = sourceVelocity.getZ();
+            sourceVelocity.setX(initialX * cos - initialZ * sin);
+            sourceVelocity.setZ(initialZ * cos + initialX * sin);
+
+            // Re-apply to the entity
+            entity.setVelocity(sourceVelocity);
+        }
+
+        return result ? SpellResult.CAST : SpellResult.NO_TARGET;
     }
 }
