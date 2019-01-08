4802f41e8d012781c5b21f8fef43007c6b529e5e
Nathan Wolf <nathan@elmakers.com>
2019-01-08 09:17:05 -0800

    Add optional "fail" action handler to Check actions

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/CheckAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/CheckAction.java
@@ -1,7 +1,10 @@
 package com.elmakers.mine.bukkit.action;
 
+import org.bukkit.configuration.ConfigurationSection;
+
 import com.elmakers.mine.bukkit.api.action.ActionHandler;
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 
 public abstract class CheckAction extends CompoundAction {
@@ -9,8 +12,20 @@ public abstract class CheckAction extends CompoundAction {
     protected abstract boolean isAllowed(CastContext context);
 
     @Override
+    protected void addHandlers(Spell spell, ConfigurationSection parameters) {
+        addHandler(spell, "actions");
+        addHandler(spell, "fail");
+    }
+
+    @Override
     public SpellResult step(CastContext context) {
         boolean allowed = isAllowed(context);
+        if (!allowed) {
+            ActionHandler fail = getHandler("fail");
+            if (fail != null && fail.size() != 0) {
+                return startActions("fail");
+            }
+        }
         ActionHandler actions = getHandler("actions");
         if (actions == null || actions.size() == 0) {
             return allowed ? SpellResult.CAST : SpellResult.STOP;
