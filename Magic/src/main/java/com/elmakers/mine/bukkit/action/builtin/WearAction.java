3c5d9e2181d235a5a4142941595e0f2716ef965c
Nathan Wolf <nathan@elmakers.com>
2019-08-27 09:37:26 -0500

    Add another check to the Wear action for air

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/WearAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/WearAction.java
@@ -196,6 +196,10 @@ public class WearAction extends BaseSpellAction
             materialName = context.getController().describeItem(wearItem);
         }
 
+        if (DefaultMaterials.isAir(wearItem.getType())) {
+            return SpellResult.NO_TARGET;
+        }
+
         ItemMeta meta = wearItem.getItemMeta();
 
         // Legacy support
