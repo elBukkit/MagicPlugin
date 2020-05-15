8db51a1aa7a3682020b292d606b1ed2d8f512da7
Nathan Wolf <nathan@elmakers.com>
2020-05-15 13:49:12 -0700

    Add some docs to the return values of CastPermissionManager

--- MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/protection/CastPermissionManager.java
+++ MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/protection/CastPermissionManager.java
@@ -10,6 +10,9 @@ import com.elmakers.mine.bukkit.api.spell.SpellTemplate;
 public interface CastPermissionManager {
     /**
      * This will perform cast permission checks for a specific location.
+     *
+     * @return false to deny cast permission, null to not care. Returning true means the cast will be allowed,
+     *     including breaking/building blocks, even if it otherwise would not be allowed.
      */
     @Nullable
     default Boolean getRegionCastPermission(Player player, SpellTemplate spell, Location location) {
@@ -20,6 +23,9 @@ public interface CastPermissionManager {
      * This will perform cast permission checks for a specific location.
      * This will override the region permission, and is generally for use inside of a player's
      * personal protected area, when it may be contained within a larger globally protected area.
+     *
+     * @return false to deny cast permission, null to not care. Returning true means the cast will be allowed,
+     *     including breaking/building blocks, even if it otherwise would not be allowed.
      */
     @Nullable
     default Boolean getPersonalCastPermission(Player player, SpellTemplate spell, Location location) {
