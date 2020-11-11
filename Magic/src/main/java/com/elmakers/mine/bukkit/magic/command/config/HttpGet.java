c688d8f68ca193b53ee64b0fe09ea3ff182e53ca
Nathan Wolf <nathan@elmakers.com>
2020-11-11 12:17:00 -0800

    Add `/mconfig example fetch` command

---
+++ Magic/src/main/java/com/elmakers/mine/bukkit/magic/command/config/HttpGet.java
@@ -0,0 +1,55 @@
+package com.elmakers.mine.bukkit.magic.command.config;
+
+import java.io.InputStream;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.bukkit.command.CommandSender;
+
+import com.elmakers.mine.bukkit.magic.MagicController;
+
+public abstract class HttpGet extends AsyncProcessor implements Runnable {
+    protected final String url;
+    protected final MagicController controller;
+    protected final CommandSender sender;
+
+    public HttpGet(MagicController controller, CommandSender sender, String url) {
+        this.controller = controller;
+        this.sender = sender;
+        this.url = url;
+    }
+
+    @Override
+    public void run() {
+        HttpsURLConnection connection;
+        try {
+            URL url = new URL(this.url);
+            connection = (HttpsURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/zip");
+            connection.setDoInput(true);
+            InputStream inputStream = connection.getInputStream();
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Empty response");
+            }
+            processResponse(inputStream);
+        } catch (Exception ex) {
+            fail(controller.getMessages().get("commands.mconfig.example.fetch.error"), "Error processing HTTP request", ex);
+            return;
+        }
+    }
+
+    protected abstract void processResponse(InputStream response);
+
+    protected void success(String message) {
+        success(controller, sender, message);
+    }
+
+    protected void fail(String message) {
+        fail(controller, sender, message);
+    }
+
+    protected void fail(String message, String errorMessage, Exception ex) {
+        fail(controller, sender, message, errorMessage, ex);
+    }
+}
