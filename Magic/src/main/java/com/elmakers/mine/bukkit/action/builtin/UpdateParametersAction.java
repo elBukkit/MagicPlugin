ea04cf652e69149909a027d9f0ee5c1771e7f196
Nathan Wolf <nathan@elmakers.com>
2021-01-22 19:34:44 -0800

    Add UpdateParameters action

---
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/UpdateParametersAction.java
@@ -0,0 +1,17 @@
+package com.elmakers.mine.bukkit.action.builtin;
+
+import com.elmakers.mine.bukkit.action.ActionHandler;
+import com.elmakers.mine.bukkit.action.CompoundAction;
+import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.spell.SpellResult;
+
+public class UpdateParametersAction extends CompoundAction
+{
+    @Override
+    public SpellResult step(CastContext context) {
+        for (ActionHandler handler : handlers.values()) {
+            handler.prepare(context, context.getSpell().getWorkingParameters());
+        }
+        return startActions();
+    }
+}
