d9a00159bd043ce6e3143c8e70f58a021cbd4084
Nathan Wolf <nathan@elmakers.com>
2021-03-11 21:17:00 -0800

    Add new goggles, beginner/student wands and mp5 from flame

---
+++ Magic/src/scripts/cleanrp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This will make a mess due to vanilla texture overrides.. :(
+# Need to add some special cases to the script.
+# php moveorphaned.php ../resource-pack/war ../resource-pack/hires
+php moveorphaned.php ../resource-pack/default ../resource-pack/hires
