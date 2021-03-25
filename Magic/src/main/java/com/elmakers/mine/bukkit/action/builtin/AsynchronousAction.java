d5f28f74da0ea9dc35cd95223edde863cc3db7b2
Nathan Wolf <nathan@elmakers.com>
2021-03-24 22:20:27 -0700

    Don't force spawning an Asynchronous action to take a full tick

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/AsynchronousAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/AsynchronousAction.java
@@ -7,19 +7,8 @@ import com.elmakers.mine.bukkit.api.spell.SpellResult;
 
 public class AsynchronousAction extends CompoundAction
 {
-    private boolean queued = false;
-
-    @Override
-    public void reset(CastContext context) {
-        super.reset(context);
-        queued = false;
-    }
-
     @Override
     public SpellResult perform(CastContext context) {
-        if (queued) {
-            return SpellResult.NO_ACTION;
-        }
         ActionHandler handler = getHandler("actions");
         if (handler == null) {
             return SpellResult.NO_ACTION;
@@ -29,7 +18,6 @@ public class AsynchronousAction extends CompoundAction
         handler = (ActionHandler)handler.clone();
         handler.reset(actionContext);
         actionContext.addHandler(handler);
-        queued = true;
-        return SpellResult.PENDING;
+        return SpellResult.CAST;
     }
 }
