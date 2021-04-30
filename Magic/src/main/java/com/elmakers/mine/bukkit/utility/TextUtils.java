4ef66181a19947a6a8c3a137940c139dadf7dacb
Nathan Wolf <nathan@elmakers.com>
2021-04-30 16:46:06 -0700

    Whoops, \w includes _

--- Magic/src/main/java/com/elmakers/mine/bukkit/utility/TextUtils.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/utility/TextUtils.java
@@ -25,7 +25,7 @@ public class TextUtils
         new DecimalFormat("#0.00"),
         new DecimalFormat("#0.000")
     };
-    private static final Pattern replacePattern = Pattern.compile("([$@][\\w_]+)([$@]?)");
+    private static final Pattern replacePattern = Pattern.compile("([$@][\\w]+)([$@]?)");
 
     enum Numeral {
         I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
