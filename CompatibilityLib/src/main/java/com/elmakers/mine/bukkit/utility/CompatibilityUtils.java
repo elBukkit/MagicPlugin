a2fd8822619b9426375625418af5d63b48457e53
Nathan Wolf <nathan@elmakers.com>
2021-06-13 19:28:54 -0700

    Clean up EffectUtils

--- CompatibilityLib/src/main/java/com/elmakers/mine/bukkit/utility/CompatibilityUtils.java
+++ CompatibilityLib/src/main/java/com/elmakers/mine/bukkit/utility/CompatibilityUtils.java
@@ -11,6 +11,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.Color;
 import org.bukkit.Effect;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Rotation;
@@ -57,6 +58,7 @@ import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.map.MapView;
@@ -2986,5 +2988,68 @@ public class CompatibilityUtils extends NMSUtils {
             return null;
         }
         return projectileType;
+    }
+
+    public static Entity spawnFireworkEffect(Material fireworkMaterial, Server server, Location location, FireworkEffect effect, int power, Vector direction, Integer expectedLifespan, Integer ticksFlown, boolean silent) {
+        Entity entity = null;
+        try {
+            if (fireworkMaterial == null) {
+                return null;
+            }
+            Object world = getHandle(location.getWorld());
+            ItemStack itemStack = new ItemStack(fireworkMaterial);
+            FireworkMeta meta = (FireworkMeta) itemStack.getItemMeta();
+            meta.addEffect(effect);
+            meta.setPower(power);
+            itemStack.setItemMeta(meta);
+
+            Object item = ItemUtils.getHandle(ItemUtils.makeReal(itemStack));
+            final Object fireworkHandle = class_EntityFireworkConstructor.newInstance(world, location.getX(), location.getY(), location.getZ(), item);
+            CompatibilityUtils.setSilent(fireworkHandle, silent);
+
+            if (direction != null) {
+                if (class_Entity_motField != null) {
+                    Object vec = class_Vec3D_constructor.newInstance(direction.getX(), direction.getY(), direction.getZ());
+                    class_Entity_motField.set(fireworkHandle, vec);
+                } else if (class_Entity_motXField != null) {
+                    class_Entity_motXField.set(fireworkHandle, direction.getX());
+                    class_Entity_motYField.set(fireworkHandle, direction.getY());
+                    class_Entity_motZField.set(fireworkHandle, direction.getZ());
+                }
+            }
+
+            if (ticksFlown != null) {
+                class_Firework_ticksFlownField.set(fireworkHandle, ticksFlown);
+            }
+            if (expectedLifespan != null) {
+                class_Firework_expectedLifespanField.set(fireworkHandle, expectedLifespan);
+            }
+
+            if (direction == null)
+            {
+                Object fireworkPacket = class_PacketSpawnEntityConstructor.newInstance(fireworkHandle, FIREWORK_TYPE);
+                Object fireworkId = class_Entity_getIdMethod.invoke(fireworkHandle);
+                Object watcher = class_Entity_getDataWatcherMethod.invoke(fireworkHandle);
+                Object metadataPacket = class_PacketPlayOutEntityMetadata_Constructor.newInstance(fireworkId, watcher, true);
+                Object statusPacket = class_PacketPlayOutEntityStatus_Constructor.newInstance(fireworkHandle, (byte)17);
+
+                Constructor<?> packetDestroyEntityConstructor = class_PacketPlayOutEntityDestroy.getConstructor(int[].class);
+                Object destroyPacket = packetDestroyEntityConstructor.newInstance(new int[] {(Integer)fireworkId});
+
+                Collection<? extends Player> players = server.getOnlinePlayers();
+                sendPacket(server, location, players, fireworkPacket);
+                sendPacket(server, location, players, metadataPacket);
+                sendPacket(server, location, players, statusPacket);
+                sendPacket(server, location, players, destroyPacket);
+                return null;
+            }
+
+            class_World_addEntityMethod.invoke(world, fireworkHandle, CreatureSpawnEvent.SpawnReason.CUSTOM);
+            entity = (Entity)class_Entity_getBukkitEntityMethod.invoke(fireworkHandle);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return entity;
     }
 }
