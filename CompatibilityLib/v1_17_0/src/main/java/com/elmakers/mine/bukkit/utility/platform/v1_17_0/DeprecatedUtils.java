0d631cdbf86278199929ca5566b63863d7f26361
Nathan Wolf <nathan@elmakers.com>
2021-06-21 00:02:20 -0700

    Allow 1.17 DeprecatedUtils to inherit from 1.15

--- CompatibilityLib/v1_17_0/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_17_0/DeprecatedUtils.java
+++ CompatibilityLib/v1_17_0/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_17_0/DeprecatedUtils.java
@@ -1,15 +1,12 @@
 package com.elmakers.mine.bukkit.utility.platform.v1_17_0;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.block.Skull;
 
 import com.elmakers.mine.bukkit.utility.platform.Platform;
-import com.elmakers.mine.bukkit.utility.platform.base.DeprecatedUtilsBase;
 
-public class DeprecatedUtils extends DeprecatedUtilsBase {
+public class DeprecatedUtils extends com.elmakers.mine.bukkit.utility.platform.v1_15.DeprecatedUtils {
     public DeprecatedUtils(Platform platform) {
         super(platform);
     }
@@ -17,11 +14,6 @@ public class DeprecatedUtils extends DeprecatedUtilsBase {
     @Override
     public void setTypeAndData(Block block, Material material, byte data, boolean applyPhysics) {
         block.setType(material, applyPhysics);
-    }
-
-    @Override
-    public Biome getBiome(Location location) {
-        return location.getWorld().getBiome(location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
 
     @Override
