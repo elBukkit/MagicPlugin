d1bab4c798edc75b536fec73a636ad7b3c693067
Nathan Wolf <nathan@elmakers.com>
2021-08-26 14:56:20 -0700

    Locatalbe should always return a non-null Location

--- MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/magic/Locatable.java
+++ MagicAPI/src/main/java/com/elmakers/mine/bukkit/api/magic/Locatable.java
@@ -1,9 +1,12 @@
 package com.elmakers.mine.bukkit.api.magic;
 
+import javax.annotation.Nonnull;
+
 import org.bukkit.Location;
 
 public interface Locatable {
     String getName();
+    @Nonnull
     Location getLocation();
     boolean isActive();
     default boolean isEnabled() {
