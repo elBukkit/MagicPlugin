fcec4ca5f73a04d7e351cf6f8de6f6f8ddbb4bbb
Nathan Wolf <nathan@elmakers.com>
2021-10-22 11:51:08 -0700

    Add commit_all parameter to Commit action. I originally planned to make this off by default, but then thought some people could possibly have their own custom commit spell, however unlikely, and I don't want to break it.

--- Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/CommitAction.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/action/builtin/CommitAction.java
@@ -4,17 +4,42 @@ import org.bukkit.configuration.ConfigurationSection;
 
 import com.elmakers.mine.bukkit.action.BaseSpellAction;
 import com.elmakers.mine.bukkit.api.action.CastContext;
+import com.elmakers.mine.bukkit.api.block.UndoList;
 import com.elmakers.mine.bukkit.api.block.UndoQueue;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 
 public class CommitAction extends BaseSpellAction {
+    private boolean commitAll;
+
     @Override
     public void prepare(CastContext context, ConfigurationSection parameters) {
         super.prepare(context, parameters);
+        commitAll = parameters.getBoolean("commit_all", true);
     }
+
 
     @Override
     public SpellResult perform(CastContext context) {
+        if (commitAll) {
+            return performAll(context);
+        }
+
+        UndoList undoList = context.getUndoList();
+        if (undoList.isEmpty()) {
+            return SpellResult.NO_ACTION;
+        }
+        boolean moreToUndo = true;
+        while (context.getWorkAllowed() > 0) {
+            context.addWork(1);
+            if (!undoList.commitNext()) {
+                moreToUndo = false;
+                break;
+            }
+        }
+        return moreToUndo ? SpellResult.PENDING : SpellResult.CAST;
+    }
+
+    public SpellResult performAll(CastContext context) {
         UndoQueue queue = context.getMage().getUndoQueue();
         int totalSize = queue.getSize();
         if (totalSize == 0) {
