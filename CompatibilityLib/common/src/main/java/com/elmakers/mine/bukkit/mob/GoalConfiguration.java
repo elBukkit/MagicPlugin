3d2b87c48007db99392fa4cebfcfa0262e29b0fb
Nathan Wolf <nathan@elmakers.com>
2021-10-22 09:35:21 -0700

    Default goals to linear ascending priority if none is specified

--- CompatibilityLib/common/src/main/java/com/elmakers/mine/bukkit/mob/GoalConfiguration.java
+++ CompatibilityLib/common/src/main/java/com/elmakers/mine/bukkit/mob/GoalConfiguration.java
@@ -15,13 +15,13 @@ public class GoalConfiguration implements Comparable<GoalConfiguration> {
     private final ConfigurationSection configuration;
     private final int priority;
 
-    private GoalConfiguration(GoalType goalType, ConfigurationSection configuration) {
+    private GoalConfiguration(GoalType goalType, ConfigurationSection configuration, int defaultPriority) {
         this.goalType = goalType;
         this.configuration = configuration;
-        priority = configuration.getInt("priority");
+        priority = configuration.getInt("priority", defaultPriority);
     }
 
-    public static GoalConfiguration fromConfiguration(ConfigurationSection parent, Object rawGoal, Logger logger, String logContext) {
+    public static GoalConfiguration fromConfiguration(ConfigurationSection parent, Object rawGoal, int defaultPriority, Logger logger, String logContext) {
         String goalKey;
         ConfigurationSection config;
         if (rawGoal instanceof String) {
@@ -50,22 +50,23 @@ public class GoalConfiguration implements Comparable<GoalConfiguration> {
             logger.warning("Invalid goal type in " + logContext + ": " + goalKey);
             return null;
         }
-        return new GoalConfiguration(goalType, config);
+        return new GoalConfiguration(goalType, config, defaultPriority);
     }
 
     @Nullable
     public static List<GoalConfiguration> fromList(ConfigurationSection parent, String key, Logger logger, String logContext) {
         if (!parent.contains(key)) return null;
         List<GoalConfiguration> list = new ArrayList<>();
+        int defaultPriority = 0;
         if (parent.isList(key)) {
             List<?> goals = parent.getList(key);
             for (Object rawGoal : goals) {
-                GoalConfiguration goal = fromConfiguration(parent, rawGoal, logger, logContext);
+                GoalConfiguration goal = fromConfiguration(parent, rawGoal, defaultPriority++, logger, logContext);
                 if (goal == null) continue;
                 list.add(goal);
             }
         } else {
-            GoalConfiguration single = fromConfiguration(parent, parent.get(key), logger, logContext);
+            GoalConfiguration single = fromConfiguration(parent, parent.get(key), defaultPriority, logger, logContext);
             list.add(single);
         }
         return list;
