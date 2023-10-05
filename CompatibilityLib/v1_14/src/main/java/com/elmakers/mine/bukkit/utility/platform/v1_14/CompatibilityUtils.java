e00a16fe78f10849e942e22753706751e6d3a46c
Nathan Wolf <nathan@elmakers.com>
2023-10-05 16:47:43 -0700

    Remove 1.13 layer

--- CompatibilityLib/v1_14/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_14/CompatibilityUtils.java
+++ CompatibilityLib/v1_14/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_14/CompatibilityUtils.java
@@ -4,17 +4,36 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
+import javax.annotation.Nonnull;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.AnaloguePowerable;
+import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.Lightable;
+import org.bukkit.block.data.Powerable;
+import org.bukkit.block.data.Waterlogged;
+import org.bukkit.block.data.type.Dispenser;
+import org.bukkit.block.data.type.Door;
+import org.bukkit.block.data.type.Piston;
 import org.bukkit.block.data.type.Slab;
+import org.bukkit.block.data.type.Snow;
 import org.bukkit.entity.AbstractArrow;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.BlastingRecipe;
 import org.bukkit.inventory.CampfireRecipe;
 import org.bukkit.inventory.CookingRecipe;
 import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.RecipeChoice;
@@ -23,13 +42,274 @@ import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.SmokingRecipe;
 import org.bukkit.inventory.StonecuttingRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.spigotmc.event.entity.EntityDismountEvent;
 
+import com.elmakers.mine.bukkit.utility.DoorActionType;
 import com.elmakers.mine.bukkit.utility.platform.Platform;
+import com.elmakers.mine.bukkit.utility.platform.PlatformInterpreter;
 
-public class CompatibilityUtils extends com.elmakers.mine.bukkit.utility.platform.v1_13.CompatibilityUtils {
+public class CompatibilityUtils extends com.elmakers.mine.bukkit.utility.platform.legacy.CompatibilityUtils {
 
     public CompatibilityUtils(Platform platform) {
         super(platform);
+    }
+
+    @Override
+    public Inventory createInventory(InventoryHolder holder, int size, final String name) {
+        size = (int) (Math.ceil((double) size / 9) * 9);
+        size = Math.min(size, 54);
+        String translatedName = translateColors(name);
+        return Bukkit.createInventory(holder, size, translatedName);
+    }
+
+    @Override
+    public boolean performDoorAction(Block[] doorBlocks, DoorActionType actionType) {
+        BlockData blockData = doorBlocks[0].getBlockData();
+        if (!(blockData instanceof Door)) {
+            return false;
+        }
+        Door doorData = (Door)blockData;
+        switch (actionType) {
+            case OPEN:
+                if (doorData.isOpen()) {
+                    return false;
+                }
+                doorData.setOpen(true);
+                break;
+            case CLOSE:
+                if (!doorData.isOpen()) {
+                    return false;
+                }
+                doorData.setOpen(false);
+                break;
+            case TOGGLE:
+                doorData.setOpen(!doorData.isOpen());
+            default:
+                return false;
+        }
+        // Going to assume we only need to update one of them?
+        doorBlocks[0].setBlockData(doorData);
+        return true;
+    }
+
+    @Override
+    public boolean checkDoorAction(Block[] doorBlocks, DoorActionType actionType) {
+        BlockData blockData = doorBlocks[0].getBlockData();
+        if (!(blockData instanceof Door)) {
+            return false;
+        }
+        Door doorData = (Door)blockData;
+        switch (actionType) {
+            case OPEN:
+                return !doorData.isOpen();
+            case CLOSE:
+                return doorData.isOpen();
+            case TOGGLE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public Block[] getDoorBlocks(Block targetBlock) {
+        BlockData blockData = targetBlock.getBlockData();
+        if (!(blockData instanceof Door)) {
+            return null;
+        }
+        Door doorData = (Door)blockData;
+        Block[] doorBlocks = new Block[2];
+        if (doorData.getHalf() == Bisected.Half.TOP) {
+            doorBlocks[1] = targetBlock;
+            doorBlocks[0] = targetBlock.getRelative(BlockFace.DOWN);
+        } else {
+            doorBlocks[1] = targetBlock.getRelative(BlockFace.UP);
+            doorBlocks[0] = targetBlock;
+        }
+        return doorBlocks;
+    }
+
+    @Override
+    public boolean setTorchFacingDirection(Block block, BlockFace facing) {
+        BlockData blockData = block.getBlockData();
+        if (!(blockData instanceof Directional)) {
+            return false;
+        }
+        Directional directional = (Directional)blockData;
+        directional.setFacing(facing);
+        block.setBlockData(directional);
+        return true;
+    }
+
+    @Override
+    public void cancelDismount(EntityDismountEvent event) {
+        event.setCancelled(true);
+    }
+
+    @Override
+    public boolean canToggleBlockPower(Block block) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null) {
+            return false;
+        }
+        if (blockData instanceof Powerable) {
+            return true;
+        }
+        if (blockData instanceof Lightable) {
+            return true;
+        }
+        if (blockData instanceof AnaloguePowerable) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean extendPiston(Block block) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null) {
+            return false;
+        }
+        if (blockData instanceof Piston) {
+            Piston piston = (Piston)blockData;
+            piston.setExtended(true);
+            block.setBlockData(piston, true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean toggleBlockPower(Block block) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null) {
+            return false;
+        }
+        if (blockData instanceof Powerable) {
+            Powerable powerable = (Powerable)blockData;
+            powerable.setPowered(!powerable.isPowered());
+            block.setBlockData(powerable, true);
+            return true;
+        }
+        if (blockData instanceof Lightable) {
+            Lightable lightable = (Lightable)blockData;
+            lightable.setLit(!lightable.isLit());
+            block.setBlockData(lightable, true);
+            return true;
+        }
+        if (blockData instanceof AnaloguePowerable) {
+            AnaloguePowerable powerable = (AnaloguePowerable)blockData;
+            powerable.setPower(powerable.getMaximumPower() - powerable.getPower());
+            block.setBlockData(powerable, true);
+            return true;
+        }
+        if (blockData instanceof Dispenser) {
+            Dispenser dispenser = (Dispenser)blockData;
+            dispenser.setTriggered(!dispenser.isTriggered());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isPowerable(Block block) {
+        BlockData blockData = block.getBlockData();
+        return blockData != null && blockData instanceof Powerable;
+    }
+
+    @Override
+    public boolean isPowered(Block block) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null || !(blockData instanceof Powerable)) return false;
+        Powerable powerable = (Powerable)blockData;
+        return powerable.isPowered();
+    }
+
+    @Override
+    public boolean setPowered(Block block, boolean powered) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null || !(blockData instanceof Powerable)) return false;
+        Powerable powerable = (Powerable)blockData;
+        powerable.setPowered(powered);
+        block.setBlockData(powerable, true);
+        return true;
+    }
+
+    @Override
+    public boolean isWaterLoggable(Block block) {
+        BlockData blockData = block.getBlockData();
+        return blockData != null && blockData instanceof Waterlogged;
+    }
+
+    @Override
+    public boolean setWaterlogged(Block block, boolean waterlogged) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null || !(blockData instanceof Waterlogged)) return false;
+        Waterlogged waterlogger = (Waterlogged)blockData;
+        waterlogger.setWaterlogged(waterlogged);
+        block.setBlockData(waterlogger, true);
+        return true;
+    }
+
+    @Override
+    public boolean setTopHalf(Block block) {
+        BlockData blockData = block.getBlockData();
+        if (blockData == null || !(blockData instanceof Bisected)) return false;
+        Bisected bisected = (Bisected)blockData;
+        bisected.setHalf(Bisected.Half.TOP);
+        block.setBlockData(bisected, false);
+        return true;
+    }
+
+    @Override
+    public boolean isFilledMap(Material material) {
+        return material == Material.FILLED_MAP;
+    }
+
+
+    @Override
+    public void setMaterialCooldown(Player player, Material material, int duration) {
+        player.setCooldown(material, duration);
+    }
+
+    @Override
+    public void sendBlockChange(Player player, Location location, Material material, String blockData) {
+        if (blockData != null) {
+            player.sendBlockChange(location, platform.getPlugin().getServer().createBlockData(blockData));
+        } else {
+            super.sendBlockChange(player, location, material, blockData);
+        }
+    }
+
+    @Override
+    @Nonnull
+    public FallingBlock spawnFallingBlock(Location location, Material material, String blockDataString) {
+        if (blockDataString != null && !blockDataString.isEmpty()) {
+            BlockData blockData = PlatformInterpreter.getPlatform().getPlugin().getServer().createBlockData(blockDataString);
+            return location.getWorld().spawnFallingBlock(location, blockData);
+        }
+
+        return super.spawnFallingBlock(location, material, blockDataString);
+    }
+
+    @Override
+    public void setSnowLevel(Block block, int level) {
+        BlockData blockData = block.getBlockData();
+        if (blockData instanceof Snow) {
+            Snow snow = (Snow)blockData;
+            snow.setLayers(level);
+            block.setBlockData(blockData);
+        }
+    }
+
+    @Override
+    public int getSnowLevel(Block block) {
+        int level = 0;
+        BlockData blockData = block.getBlockData();
+        if (blockData instanceof Snow) {
+            Snow snow = (Snow)blockData;
+            level = snow.getLayers();
+        }
+        return level;
     }
 
     @Override
