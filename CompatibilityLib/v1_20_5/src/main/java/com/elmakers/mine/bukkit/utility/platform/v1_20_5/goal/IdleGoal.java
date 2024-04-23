3ec0e19c78ba785b0e7acbb9cea05dfc84daf7b2
Nathan Wolf <nathan@elmakers.com>
2024-04-23 13:15:38 -0700

    Add 1.20.5 compatibility layer. Currently does not build, lots of major changes in this version.

---
+++ CompatibilityLib/v1_20_5/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_20_5/goal/IdleGoal.java
@@ -0,0 +1,10 @@
+package com.elmakers.mine.bukkit.utility.platform.v1_20_5.goal;
+
+import net.minecraft.world.entity.ai.goal.Goal;
+
+public class IdleGoal extends Goal {
+    @Override
+    public boolean canUse() {
+        return true;
+    }
+}
