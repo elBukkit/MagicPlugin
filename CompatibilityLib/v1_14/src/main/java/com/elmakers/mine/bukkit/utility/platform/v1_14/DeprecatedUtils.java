8542b178e965307573f96d768b9ca84d3bcd4100
Nathan Wolf <nathan@elmakers.com>
2025-02-10 16:46:00 -0800

    Fix (remove) banner base color methods in modern MC versions

--- CompatibilityLib/v1_14/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_14/DeprecatedUtils.java
+++ CompatibilityLib/v1_14/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_14/DeprecatedUtils.java
@@ -1,5 +1,7 @@
 package com.elmakers.mine.bukkit.utility.platform.v1_14;
 
+import org.bukkit.DyeColor;
+import org.bukkit.inventory.meta.BannerMeta;
 import org.bukkit.map.MapView;
 
 import com.elmakers.mine.bukkit.utility.platform.Platform;
@@ -15,5 +17,15 @@ public class DeprecatedUtils extends com.elmakers.mine.bukkit.utility.platform.l
         // MapView id is now an int- we proabably should update our own code
         // and change this to an int
         return (short)mapView.getId();
+    }
+
+    @Override
+    public DyeColor getBaseColor(BannerMeta banner) {
+        return DyeColor.WHITE;
+    }
+
+    @Override
+    public void setBaseColor(BannerMeta banner, DyeColor color) {
+        // Can't actually do this anymore, different banner colors are different materials
     }
 }
