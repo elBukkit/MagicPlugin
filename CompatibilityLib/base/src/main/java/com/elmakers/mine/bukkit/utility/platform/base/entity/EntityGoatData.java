0c1cf6050cf77c741207d2b3edde1d4b6c92228a
Nathan Wolf <nathan@elmakers.com>
2026-03-11 20:02:21 +0100

    Altering a goat will change its horns

--- CompatibilityLib/base/src/main/java/com/elmakers/mine/bukkit/utility/platform/base/entity/EntityGoatData.java
+++ CompatibilityLib/base/src/main/java/com/elmakers/mine/bukkit/utility/platform/base/entity/EntityGoatData.java
@@ -9,10 +9,14 @@ import com.elmakers.mine.bukkit.utility.ConfigUtils;
 
 public class EntityGoatData extends EntityAnimalData {
     public Boolean screaming;
+    public Boolean leftHorn;
+    public Boolean rightHorn;
 
     public EntityGoatData(ConfigurationSection parameters, MageController controller) {
         super(parameters, controller);
         screaming = ConfigUtils.getOptionalBoolean(parameters, "screaming");
+        leftHorn = ConfigUtils.getOptionalBoolean(parameters, "horn_left");
+        rightHorn = ConfigUtils.getOptionalBoolean(parameters, "horn_right");
     }
 
     public EntityGoatData(Entity entity) {
@@ -20,15 +24,19 @@ public class EntityGoatData extends EntityAnimalData {
         if (entity instanceof Goat) {
             Goat goat = (Goat)entity;
             screaming = goat.isScreaming();
+            rightHorn = goat.hasRightHorn();
+            leftHorn = goat.hasLeftHorn();
         }
     }
 
     @Override
     public void apply(Entity entity) {
         super.apply(entity);
-        if (entity instanceof Goat && screaming != null) {
+        if (entity instanceof Goat) {
             Goat goat = (Goat)entity;
-            goat.setScreaming(screaming);
+            if (screaming != null) goat.setScreaming(screaming);
+            if (rightHorn != null) goat.setScreaming(rightHorn);
+            if (leftHorn != null) goat.setScreaming(leftHorn);
         }
     }
 
@@ -39,6 +47,12 @@ public class EntityGoatData extends EntityAnimalData {
         }
         Goat goat = (Goat)entity;
         goat.setScreaming(!goat.isScreaming());
+        if (goat.isScreaming()) {
+            int hornMask = (goat.hasRightHorn() ? 1 : 0) | (goat.hasLeftHorn() ? 2 : 0);
+            hornMask = (hornMask + 1) & 3;
+            goat.setRightHorn((hornMask & 1) == 1);
+            goat.setLeftHorn((hornMask & 2) == 2);
+        }
         return true;
     }
 
