bd43613ebbe461c25904f7d0a90a0fac34f837c6
Nathan Wolf <nathan@elmakers.com>
2026-04-25 17:34:23 +0200

    Remove 1.12 entity utils

--- CompatibilityLib/v1_12/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_12/entity/EntityMuleData.java
+++
@@ -1,9 +0,0 @@
-package com.elmakers.mine.bukkit.utility.platform.v1_12.entity;
-
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Entity;
-
-import com.elmakers.mine.bukkit.api.magic.MageController;
-
-public class EntityMuleData extends EntityChestedHorseData {
-}
