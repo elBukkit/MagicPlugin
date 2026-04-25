4db4a7c73a0e27db6c4c71333b2ed991015c137a
Nathan Wolf <nathan@elmakers.com>
2026-04-25 17:19:03 +0200

    Collapse ModernDeprecatedUtils down one level to 1_15

--- CompatibilityLib/modern/src/main/java/com/elmakers/mine/bukkit/utility/platform/modern/ModernDeprecatedUtils.java
+++ CompatibilityLib/modern/src/main/java/com/elmakers/mine/bukkit/utility/platform/modern/ModernDeprecatedUtils.java
@@ -1,9 +1,5 @@
 package com.elmakers.mine.bukkit.utility.platform.modern;
 
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.Skull;
-
 import com.elmakers.mine.bukkit.utility.platform.Platform;
 
 public class ModernDeprecatedUtils extends com.elmakers.mine.bukkit.utility.platform.v1_15.DeprecatedUtils {
@@ -11,17 +7,4 @@ public class ModernDeprecatedUtils extends com.elmakers.mine.bukkit.utility.plat
         super(platform);
     }
 
-    @Override
-    public void setTypeAndData(Block block, Material material, byte data, boolean applyPhysics) {
-        block.setType(material, applyPhysics);
-    }
-
-    @Override
-    public void setSkullType(Skull skullBlock, short skullType) {
-    }
-
-    @Override
-    public short getSkullType(Skull skullBlock) {
-        return 0;
-    }
 }
--- CompatibilityLib/v1_15/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_15/DeprecatedUtils.java
+++ CompatibilityLib/v1_15/src/main/java/com/elmakers/mine/bukkit/utility/platform/v1_15/DeprecatedUtils.java
@@ -1,7 +1,10 @@
 package com.elmakers.mine.bukkit.utility.platform.v1_15;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.block.Skull;
 
 import com.elmakers.mine.bukkit.utility.platform.Platform;
 
@@ -14,5 +17,19 @@ public class DeprecatedUtils extends com.elmakers.mine.bukkit.utility.platform.v
     @Override
     public Biome getBiome(Location location) {
         return location.getWorld().getBiome(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    @Override
+    public void setTypeAndData(Block block, Material material, byte data, boolean applyPhysics) {
+        block.setType(material, applyPhysics);
+    }
+
+    @Override
+    public void setSkullType(Skull skullBlock, short skullType) {
+    }
+
+    @Override
+    public short getSkullType(Skull skullBlock) {
+        return 0;
     }
 }
