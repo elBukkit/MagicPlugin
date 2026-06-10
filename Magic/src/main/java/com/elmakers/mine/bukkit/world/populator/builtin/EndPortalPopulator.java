63f390f71d3d55440e4e33d4d24464a007ae193c
Nathan Wolf <nathan@elmakers.com>
2026-06-10 08:19:07 +0200

    Separate out the tower and exit populators

---
+++ Magic/src/main/java/com/elmakers/mine/bukkit/world/populator/builtin/EndPortalPopulator.java
@@ -0,0 +1,79 @@
+package com.elmakers.mine.bukkit.world.populator.builtin;
+
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.block.EndGateway;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.generator.LimitedRegion;
+import org.bukkit.generator.WorldInfo;
+
+import com.elmakers.mine.bukkit.utility.random.RandomUtils;
+import com.elmakers.mine.bukkit.world.populator.MagicBlockPopulator;
+
+public class EndPortalPopulator extends MagicBlockPopulator {
+    private int minPortalWidth = 3;
+    private int maxPortalWidth = 3;
+    private int minTunnelHeight = 0;
+    private int maxTunnelHeight = 0;
+    private boolean gateway = false;
+
+    @Override
+    public boolean onLoad(ConfigurationSection config) {
+        minPortalWidth = config.getInt("min_portal_width", minPortalWidth);
+        maxPortalWidth = config.getInt("max_portal_width", maxPortalWidth);
+        minTunnelHeight = config.getInt("min_tunnel_height", minPortalWidth);
+        maxTunnelHeight = config.getInt("max_tunnel_height", maxPortalWidth);
+        gateway = config.getBoolean("gateway", gateway);
+        return true;
+    }
+
+    private BlockData getGatewayBlock() {
+        BlockData gatewayData = getController().getPlugin().getServer().createBlockData(Material.END_GATEWAY);
+        if (gatewayData instanceof EndGateway) {
+            EndGateway gateway = (EndGateway)gatewayData;
+            gateway.setAge(-Integer.MAX_VALUE);
+        }
+        return gatewayData;
+    }
+
+    @Override
+    public void populate(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, LimitedRegion region) {
+        final int chunkGlobalX = chunkX << 4;
+        final int chunkGlobalZ = chunkZ << 4;
+        final int floorLevel = world.getGroundLevel();
+        final int portalWidth = RandomUtils.range(random, minPortalWidth, maxPortalWidth);
+        final int tunnelHeight = RandomUtils.range(random, minTunnelHeight, maxTunnelHeight);
+        final int portalLeft = 8 - (int)Math.ceil((double)portalWidth / 2);
+        final int portalRight = 8 + (int)Math.floor((double)portalWidth / 2);
+
+        final int buffer = region.getBuffer();
+        final int minX = chunkGlobalX - buffer;
+        final int maxX = chunkGlobalX + 16 + buffer;
+        final int minZ = chunkGlobalZ - buffer;
+        final int maxZ = chunkGlobalZ + 16 + buffer;
+        final int minExitX = chunkGlobalX + portalLeft;
+        final int maxExitX = chunkGlobalX + portalRight;
+        final int minExitZ = chunkGlobalZ + portalLeft;
+        final int maxExitZ = chunkGlobalZ + portalRight;
+        final int maxExitY = floorLevel + tunnelHeight;
+        final BlockData exitBlock = gateway ? getGatewayBlock() : null;
+
+        for (int x = minX; x < maxX; x++) {
+            for (int z = minZ; z < maxZ; z++) {
+                for (int y = floorLevel + 1; y <= maxExitY; y++) {
+                    if (exitBlock != null && x >= minExitX && x <= maxExitX && z >= minExitZ && z <= maxExitZ) {
+                        region.setBlockData(x, y, z, exitBlock);
+                    } else {
+                        region.setType(x, y, z, Material.AIR);
+                    }
+                }
+
+                if (x >= minExitX && x <= maxExitX && z >= minExitZ && z <= maxExitZ) {
+                    region.setType(x, floorLevel, z, Material.END_PORTAL);
+                }
+            }
+        }
+    }
+}
--- Magic/src/main/java/com/elmakers/mine/bukkit/world/populator/builtin/TowerPopulator.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/world/populator/builtin/TowerPopulator.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-import org.bukkit.Material;
-import org.bukkit.block.EndGateway;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.generator.LimitedRegion;
@@ -19,12 +17,12 @@ import com.elmakers.mine.bukkit.utility.random.RandomUtils;
 import com.elmakers.mine.bukkit.world.populator.MagicBlockPopulator;
 
 public class TowerPopulator extends MagicBlockPopulator {
-    private double exitProbability;
     private List<MaterialAndData> wallBlocks = Collections.emptyList();
+    private int minHeight = 32;
+    private int maxHeight = 128;
 
     @Override
     public boolean onLoad(ConfigurationSection config) {
-        exitProbability = config.getDouble("exit_probability");
         MagicController controller = world.getController();
         MaterialSet wallSet = controller.getMaterialSetManager().fromConfig(config, "blocks");
         if (wallSet == null) {
@@ -34,57 +32,30 @@ public class TowerPopulator extends MagicBlockPopulator {
         if (wallSet != null) {
             wallBlocks = new ArrayList<>(wallSet.getMaterialsWithData());
         }
+        minHeight = config.getInt("min_height", minHeight);
+        maxHeight = config.getInt("max_height", maxHeight);
         return true;
-    }
-
-    private BlockData getWindowBlock() {
-        BlockData gatewayData = getController().getPlugin().getServer().createBlockData(Material.END_GATEWAY);
-        if (gatewayData instanceof EndGateway) {
-            EndGateway gateway = (EndGateway)gatewayData;
-            gateway.setAge(-Integer.MAX_VALUE);
-        }
-        return gatewayData;
     }
 
     @Override
     public void populate(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, LimitedRegion region) {
-        final int maxHeight = worldInfo.getMaxHeight();
+        final int worldHeight = worldInfo.getMaxHeight();
+        final int towerHeight = Math.min(worldHeight - 1, RandomUtils.range(random, minHeight, maxHeight));
         final int chunkGlobalX = chunkX << 4;
         final int chunkGlobalZ = chunkZ << 4;
-        final int floorLevel = 0; // room.getFloorLevel();
+        final int floorLevel = world.getGroundLevel();
 
         final int buffer = region.getBuffer();
         final int minX = chunkGlobalX - buffer;
         final int maxX = chunkGlobalX + 16 + buffer;
         final int minZ = chunkGlobalZ - buffer;
         final int maxZ = chunkGlobalZ + 16 + buffer;
-        final boolean isExit = random.nextDouble() < exitProbability;
-        final int minExitX = chunkGlobalX + 6;
-        final int maxExitX = chunkGlobalX + 9;
-        final int minExitZ = chunkGlobalZ + 6;
-        final int maxExitZ = chunkGlobalZ + 9;
-        final int maxExitY = floorLevel + 6;
-
-        final BlockData exitBlock = isExit ? getWindowBlock() : null;
-
         final MaterialAndData wallBlock = RandomUtils.getRandom(wallBlocks, random);
         final BlockData wallBlockData = wallBlock.createBlockData();
         for (int x = minX; x < maxX; x++) {
             for (int z = minZ; z < maxZ; z++) {
-                for (int y = floorLevel + 1; y < maxHeight; y++) {
-                    if (exitBlock != null && y <= maxExitY && ((x >= minExitX && x <= maxExitX) || (z >= minExitZ && z <= maxExitZ))) {
-                        if (x >= minExitX && x <= maxExitX && z >= minExitZ && z <= maxExitZ) {
-                            region.setBlockData(x, y, z, exitBlock);
-                        } else {
-                            region.setType(x, y, z, Material.AIR);
-                        }
-                    } else {
-                        region.setBlockData(x, y, z, wallBlockData);
-                    }
-                }
-
-                if (exitBlock != null && x >= minExitX && x <= maxExitX && z >= minExitZ && z <= maxExitZ) {
-                    region.setType(x, floorLevel, z, Material.END_PORTAL);
+                for (int y = floorLevel + 1; y < towerHeight; y++) {
+                    region.setBlockData(x, y, z, wallBlockData);
                 }
             }
         }
