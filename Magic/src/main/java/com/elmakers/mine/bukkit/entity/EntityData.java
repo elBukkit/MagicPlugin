8b84805a04e6153c0fd156cb30867c4e31be0e95
Nathan Wolf <nathan@elmakers.com>
2026-06-19 13:29:33 +0200

    Fix using equations in entity_attributes

--- Magic/src/main/java/com/elmakers/mine/bukkit/entity/EntityData.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/entity/EntityData.java
@@ -555,7 +555,7 @@ public class EntityData
             }
             for (String attributeKey : keys) {
                 try {
-                    if (attributeConfiguration.isString(attributeKey)) continue;
+                    if (attributeConfiguration.isString(attributeKey) && attributeConfiguration.getString(attributeKey).isEmpty()) continue;
                     Attribute attribute = Attribute.valueOf(attributeKey.toUpperCase());
                     attributes.put(attribute, attributeConfiguration.getDouble(attributeKey));
                 } catch (Exception ex) {
