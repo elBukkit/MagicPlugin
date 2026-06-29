e323da53d7f19006bea0b66eab6ef72a8ba23583
Nathan Wolf <nathan@elmakers.com>
2026-06-29 12:33:45 +0200

    Add lights to the Pools populator.
    I was going to make a separate light populator, but it would be hard to make them aligned to the pools.

--- Magic/src/main/java/com/elmakers/mine/bukkit/world/populator/builtin/PoolsPopulator.java
+++ Magic/src/main/java/com/elmakers/mine/bukkit/world/populator/builtin/PoolsPopulator.java
@@ -1,5 +1,7 @@
 package com.elmakers.mine.bukkit.world.populator.builtin;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Material;
@@ -9,19 +11,26 @@ import org.bukkit.generator.LimitedRegion;
 import org.bukkit.generator.WorldInfo;
 import org.jetbrains.annotations.NotNull;
 
+import com.elmakers.mine.bukkit.api.block.MaterialAndData;
 import com.elmakers.mine.bukkit.utility.random.IntegerRange;
+import com.elmakers.mine.bukkit.utility.random.RandomUtils;
 import com.elmakers.mine.bukkit.world.populator.BaseBlockPopulator;
 
 public class PoolsPopulator extends BaseBlockPopulator {
     private IntegerRange depth;
     private IntegerRange walkwayWidth;
     private double islandProbability = 0.75;
+    private double lightProbability = 1;
+    private List<MaterialAndData> lightBlocks = Collections.emptyList();
 
     @Override
     public boolean onLoad(ConfigurationSection config) {
-        depth = IntegerRange.fromConfig(getLogger(), config, "water_depth", 1, 1);
+        depth = IntegerRange.fromConfig(getLogger(), config, "depth", 1, 1);
         walkwayWidth = IntegerRange.fromConfig(getLogger(), config, "walkway_width", 0, 10);
         islandProbability = config.getDouble("island_probability", islandProbability);
+        lightProbability = config.getDouble("light_probability", lightProbability);
+        lightBlocks = parseBlocks(config, "light_blocks", "all_lights");
+
         return true;
     }
 
@@ -34,6 +43,9 @@ public class PoolsPopulator extends BaseBlockPopulator {
         final int walkwayLeft = 8 - walkwayWidthHalf;
         final int walkWayRight = 8 + walkwayWidthHalf;
         final boolean hasIsland = !isStartingChunk && random.nextDouble() < islandProbability;
+        final int lightsFirst = walkwayLeft / 2 + 1;
+        final int lightsSecond = 16 - lightsFirst;
+        final BlockData lightBlock = RandomUtils.getRandom(lightBlocks, random).createBlockData();
         final int chunkGlobalX = chunkX << 4;
         final int chunkGlobalZ = chunkZ << 4;
 
@@ -48,6 +60,11 @@ public class PoolsPopulator extends BaseBlockPopulator {
                 final boolean isIsland = hasIsland && dx >= 7 && dz >= 7 && dx <= 9 && dz <= 9;
 
                 if (!isWalkway && !isIsland) {
+                    final boolean isCenterLight = (dx == lightsFirst || dx == lightsSecond) && (dz == lightsFirst || dz == lightsSecond);
+                    if (isCenterLight && random.nextDouble() < lightProbability) {
+                        region.setBlockData(x, waterMinY, z, lightBlock);
+                    }
+
                     for (int y = floorLevel; y > waterMinY; y--) {
                         region.setBlockData(x, y, z, waterBlock);
                     }
